61d50bb11a547c4aae0796b026dcb0d5eca8588a
dkflfkd53 <b57062562@gmail.com>
2024-01-14 13:42:10 +0900

    first commit

---
+++ src/main/java/com/example/daedongyeojido_server/DaedongyeojidoServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.daedongyeojido_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaedongyeojidoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaedongyeojidoServerApplication.class, args);
+	}
+
+}
