5e35f5704a8c0494820d259c98777c1d6143267d
dkflfkd53 <b57062562@gmail.com>
2024-01-15 12:54:00 +0900

    🌧 :: PropertiesScan 어노테이션 추가

--- src/main/java/com/example/daedongyeojido_server/DaedongyeojidoServerApplication.java
+++ src/main/java/com/example/daedongyeojido_server/DaedongyeojidoServerApplication.java
@@ -2,8 +2,11 @@ package com.example.daedongyeojido_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class DaedongyeojidoServerApplication {
 
 	public static void main(String[] args) {
