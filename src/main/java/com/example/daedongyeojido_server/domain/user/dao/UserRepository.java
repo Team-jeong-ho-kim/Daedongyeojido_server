7875a149884a9716a346830299e0a29617b171f0
dkflfkd53 <b57062562@gmail.com>
2024-01-15 13:12:00 +0900

    🌧 :: findByClassNumber 메서드 추가

--- src/main/java/com/example/daedongyeojido_server/domain/user/dao/UserRepository.java
+++ src/main/java/com/example/daedongyeojido_server/domain/user/dao/UserRepository.java
@@ -3,7 +3,9 @@ package com.example.daedongyeojido_server.domain.user.dao;
 import com.example.daedongyeojido_server.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByClassNumber(String classNumber);
 }
