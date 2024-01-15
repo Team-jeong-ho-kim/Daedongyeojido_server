ab2318bac0a4ae56106e6d34363b94d3287047ae
dkflfkd53 <b57062562@gmail.com>
2024-01-15 13:13:00 +0900

    🌧 :: CustomUserDetails 추가

---
+++ src/main/java/com/example/daedongyeojido_server/global/security/auth/CustomUserDetails.java
@@ -0,0 +1,45 @@
+package com.example.daedongyeojido_server.global.security.auth;
+
+import com.example.daedongyeojido_server.domain.user.domain.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public record CustomUserDetails(User user) implements UserDetails {
+
+    @Override
+    public String getUsername() {
+        return user.getClassNumber();
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<>(Collections.singleton(new SimpleGrantedAuthority("ROLE_"+user.getPart().toString())));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
