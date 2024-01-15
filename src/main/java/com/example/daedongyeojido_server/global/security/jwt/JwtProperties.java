a58fb6a78c239469b32b4f0bedbac11c1c37967b
dkflfkd53 <b57062562@gmail.com>
2024-01-15 13:16:48 +0900

    🌧 :: JwtProperties 추가

---
+++ src/main/java/com/example/daedongyeojido_server/global/security/jwt/JwtProperties.java
@@ -0,0 +1,23 @@
+package com.example.daedongyeojido_server.global.security.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@AllArgsConstructor
+//@ConstructorBinding
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+
+    private final String header;
+
+    private final String prefix;
+
+    private final String secret;
+
+    private final Long accessExpiration;
+
+    private final Long refreshExpiration;
+
+}
