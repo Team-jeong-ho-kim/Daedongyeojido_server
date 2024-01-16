2d7e1244ce15ce5b13e1b35856958c4efd328027
dkflfkd53 <b57062562@gmail.com>
2024-01-16 13:13:44 +0900

    ♻️ :: User entity 수정

--- src/main/java/com/example/daedongyeojido_server/domain/user/domain/User.java
+++ src/main/java/com/example/daedongyeojido_server/domain/user/domain/User.java
@@ -5,12 +5,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.util.List;
 import java.util.UUID;
@@ -23,6 +25,12 @@ import java.util.UUID;
 public class User {
 
     @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid")
+    @Column(
+            columnDefinition = "BINARY(16)",
+            nullable = false
+    )
     private UUID userId;
 
     @Column(name = "class_number", length = 4, nullable = false)
@@ -31,12 +39,6 @@ public class User {
     @Column(name = "name", length = 4, nullable = false)
     private String name;
 
-    @Column(name = "account_id", length = 20, nullable = false)
-    private String accountId;
-
-    @Column(name = "password", length = 20)
-    private String password;
-
     @Enumerated(EnumType.STRING)
     @Column(name = "part", nullable = false)
     private Part part;
@@ -44,7 +46,7 @@ public class User {
 //    @Column(name = "alarms")
 //    private List<Alarm> alarms;
 
-    @Column
+    @Column(name = "my_club", length = 20, nullable = false)
     private String myClub;
 
 }
