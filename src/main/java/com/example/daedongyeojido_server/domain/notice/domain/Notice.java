15c74f937a6220fded3019dc307bf306641efd3f
dkflfkd53 <b57062562@gmail.com>
2024-01-18 18:30:38 +0900

    ♻️ :: 공고 entity에 동아리 연관관계 매핑

--- src/main/java/com/example/daedongyeojido_server/domain/notice/domain/Notice.java
+++ src/main/java/com/example/daedongyeojido_server/domain/notice/domain/Notice.java
@@ -1,11 +1,15 @@
 package com.example.daedongyeojido_server.domain.notice.domain;
 
+import com.example.daedongyeojido_server.domain.club.domain.Club;
 import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,13 +29,19 @@ public class Notice {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "notice_id", nullable = false)
     private Long noticeId;
 
+    @Column(name = "content", length = 20, nullable = false)
     private String content;
 
     @ElementCollection
     private List<String> techStack = new ArrayList<>();
 
-    @Column(name = "deadline")
+    @Column(name = "deadline", nullable = false)
     private LocalDateTime deadline;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "club_name", nullable = false)
+    private Club clubName;
 }
