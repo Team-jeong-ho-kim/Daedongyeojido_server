86e082c72228cfa738ea8b5e0e44ff2b074143d7
dkflfkd53 <b57062562@gmail.com>
2024-01-18 19:36:14 +0900

    ♻️ :: ClubController url 수정

--- src/main/java/com/example/daedongyeojido_server/domain/club/api/ClubController.java
+++ src/main/java/com/example/daedongyeojido_server/domain/club/api/ClubController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/club")
+@RequestMapping("/jung-ho/club")
 public class ClubController {
 
     private final CreateClubService createClubService;
