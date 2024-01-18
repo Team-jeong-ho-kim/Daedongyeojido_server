0f02bb614c72cad372b28f37c36aca54f9b64d16
dkflfkd53 <b57062562@gmail.com>
2024-01-18 18:45:04 +0900

    ♻️ :: authController url 수정

--- src/main/java/com/example/daedongyeojido_server/domain/auth/api/AuthController.java
+++ src/main/java/com/example/daedongyeojido_server/domain/auth/api/AuthController.java
@@ -7,15 +7,17 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/auth")
 public class AuthController {
 
     private final StudentLoginService studentLoginService;
 
-    @PostMapping("/login")
+    @PostMapping("/student")
     public TokenResponse studentLogin(@RequestBody @Valid StudentLoginRequest request) {
         return studentLoginService.studentLogin(request);
     }
