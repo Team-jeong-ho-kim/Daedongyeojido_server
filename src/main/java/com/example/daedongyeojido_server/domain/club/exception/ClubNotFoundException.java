d8e4620af8b004e665caf4fcd2cca6298247d0e5
dkflfkd53 <b57062562@gmail.com>
2024-01-18 18:40:34 +0900

    🌧 :: ClubNotFoundException 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/club/exception/ClubNotFoundException.java
@@ -0,0 +1,14 @@
+package com.example.daedongyeojido_server.domain.club.exception;
+
+import com.example.daedongyeojido_server.global.error.exception.BusinessException;
+import com.example.daedongyeojido_server.global.error.exception.ErrorCode;
+
+public class ClubNotFoundException extends BusinessException {
+
+    public static final BusinessException EXCEPTION = new ClubNotFoundException();
+
+    public ClubNotFoundException() {
+        super(ErrorCode.CLUB_NOT_FOUND);
+    }
+
+}
