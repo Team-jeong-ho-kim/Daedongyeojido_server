13974d575bd362816df1660162d72ef53f27896f
dkflfkd53 <b57062562@gmail.com>
2024-01-19 15:10:57 +0900

    ♻️ :: 동아리 생성 api 학번 중복 예외 추가

--- src/main/java/com/example/daedongyeojido_server/domain/club/application/CreateClubService.java
+++ src/main/java/com/example/daedongyeojido_server/domain/club/application/CreateClubService.java
@@ -3,6 +3,7 @@ package com.example.daedongyeojido_server.domain.club.application;
 import com.example.daedongyeojido_server.domain.club.dao.ClubRepository;
 import com.example.daedongyeojido_server.domain.club.domain.Club;
 import com.example.daedongyeojido_server.domain.club.dto.request.CreateClubRequest;
+import com.example.daedongyeojido_server.domain.club.exception.ExistClubException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,8 @@ public class CreateClubService {
     private final ClubRepository clubRepository;
 
     public void createClub(CreateClubRequest request) {
+        if(clubRepository.existsById(request.getClubName())) throw ExistClubException.EXCEPTION;
+
         clubRepository.save(
                 Club.builder()
                         .clubName(request.getClubName())
