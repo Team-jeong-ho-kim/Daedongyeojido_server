007fd0428c27755544f95615ef72195d18918ec4
dkflfkd53 <b57062562@gmail.com>
2024-01-19 14:08:26 +0900

    🌧 :: 분야 enum 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/notice/domain/enums/Major.java
@@ -0,0 +1,31 @@
+package com.example.daedongyeojido_server.domain.notice.domain.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Major {
+
+    FRONT("프론트엔드"),
+
+    BACK("백엔드"),
+
+    IOS("아이오에스"),
+
+    AND("안드로이드"),
+
+    FLUTTER("플러터"),
+
+    EMBEDDED("임베디드"),
+
+    AI("인공지능"),
+
+    SECURITY("보안"),
+
+    DEVOPS("데브옵스"),
+
+    DESIGN("디자인");
+
+    private final String name;
+}
