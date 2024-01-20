5e80e56f4ec58e441e9d08ea05bc83395c45db21
dkflfkd53 <b57062562@gmail.com>
2024-01-20 23:33:10 +0900

    🌧 :: 동아리 전체 조회 api controller 추가

--- src/main/java/com/example/daedongyeojido_server/domain/club/api/ClubController.java
+++ src/main/java/com/example/daedongyeojido_server/domain/club/api/ClubController.java
@@ -1,13 +1,18 @@
 package com.example.daedongyeojido_server.domain.club.api;
 
 import com.example.daedongyeojido_server.domain.club.application.CreateClubService;
+import com.example.daedongyeojido_server.domain.club.application.QueryAllClubService;
+import com.example.daedongyeojido_server.domain.club.domain.Club;
 import com.example.daedongyeojido_server.domain.club.dto.request.CreateClubRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,8 +21,15 @@ public class ClubController {
 
     private final CreateClubService createClubService;
 
+    private final QueryAllClubService queryAllClubService;
+
     @PostMapping()
     public void createClub(@RequestBody @Valid CreateClubRequest request) {
         createClubService.createClub(request);
+    }
+
+    @GetMapping("/all")
+    public List<Club> queryAllClub() {
+        return queryAllClubService.queryAllCLub();
     }
 }
