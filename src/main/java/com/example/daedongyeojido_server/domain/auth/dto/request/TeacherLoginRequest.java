654083e37d5b460b60dab38cfa78c22f016d0ba1
dkflfkd53 <b57062562@gmail.com>
2024-01-22 15:55:08 +0900

    🌧 :: 선생님 로그인 api request 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/auth/dto/request/TeacherLoginRequest.java
@@ -0,0 +1,16 @@
+package com.example.daedongyeojido_server.domain.auth.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TeacherLoginRequest {
+
+    @NotBlank
+    @Size(min = 3, max = 4, message = "name은 3글자 이상, 4글자 이하여야 합니다.")
+    private String name;
+}
