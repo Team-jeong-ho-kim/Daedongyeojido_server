c429e21bfa32374d1a0b6141d349f62dea55e746
dkflfkd53 <b57062562@gmail.com>
2024-01-22 15:45:48 +0900

    🌧 :: 회원가입 api controller 추가

--- src/main/java/com/example/daedongyeojido_server/domain/auth/api/AuthController.java
+++ src/main/java/com/example/daedongyeojido_server/domain/auth/api/AuthController.java
@@ -1,6 +1,8 @@
 package com.example.daedongyeojido_server.domain.auth.api;
 
+import com.example.daedongyeojido_server.domain.auth.application.SignupService;
 import com.example.daedongyeojido_server.domain.auth.application.StudentLoginService;
+import com.example.daedongyeojido_server.domain.auth.dto.request.SignupRequest;
 import com.example.daedongyeojido_server.domain.auth.dto.request.StudentLoginRequest;
 import com.example.daedongyeojido_server.domain.auth.dto.response.TokenResponse;
 import jakarta.validation.Valid;
@@ -15,7 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
 
+    private final SignupService signupService;
+
     private final StudentLoginService studentLoginService;
+
+    @PostMapping("/signup")
+    public void signup(@RequestBody @Valid SignupRequest request) {
+        signupService.signup(request);
+    }
 
     @PostMapping("/student")
     public TokenResponse studentLogin(@RequestBody @Valid StudentLoginRequest request) {
