d4526d7bbf84c1a655de8a0dd0d90609c5a3c517
김가은 <kge02160@gmail.com>
2024-01-24 22:07:03 +0900

    🌧 :: clubController 추가

--- src/main/java/com/example/daedongyeojido_server/domain/club/api/ClubController.java
+++ src/main/java/com/example/daedongyeojido_server/domain/club/api/ClubController.java
@@ -1,35 +1,56 @@
 package com.example.daedongyeojido_server.domain.club.api;
 
-import com.example.daedongyeojido_server.domain.club.application.CreateClubService;
-import com.example.daedongyeojido_server.domain.club.application.QueryAllClubService;
+import com.example.daedongyeojido_server.domain.club.application.*;
 import com.example.daedongyeojido_server.domain.club.domain.Club;
 import com.example.daedongyeojido_server.domain.club.dto.request.CreateClubRequest;
+import com.example.daedongyeojido_server.domain.club.dto.request.UpdateClubRequest;
+import com.example.daedongyeojido_server.domain.club.dto.response.ClubInfoResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
 @RequiredArgsConstructor
 @RequestMapping("/jung-ho/club")
+@RestController
 public class ClubController {
 
     private final CreateClubService createClubService;
-
+    private final ModifyClubService modifyClubService;
+    private final ClubInfoService clubInfoService;
     private final QueryAllClubService queryAllClubService;
+    private final DeleteClubService deleteClubService;
 
-    @PostMapping()
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/create")
     public void createClub(@RequestBody @Valid CreateClubRequest request) {
         createClubService.createClub(request);
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/all")
     public List<Club> queryAllClub() {
         return queryAllClubService.queryAllCLub();
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/detailed")
+    public ClubInfoResponse clubInfo() {
+        return clubInfoService.clubInfo();
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PatchMapping("/update")
+    public void modifyClub(@PathVariable String clubName,
+                           @RequestBody @Valid UpdateClubRequest request) {
+        modifyClubService.modifyClub(clubName, request);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/delete")
+    public void deleteClub(@PathVariable String clubName) {
+        deleteClubService.deleteClub(clubName);
     }
 }
