54f0ec515e45ac39acae221154b9eb061a8c9bea
김가은 <kge02160@gmail.com>
2024-01-24 21:52:08 +0900

    🌧 :: clubInfoResponse 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/club/dto/response/ClubInfoResponse.java
@@ -0,0 +1,13 @@
+package com.example.daedongyeojido_server.domain.club.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class ClubInfoResponse {
+
+    private String clubName;
+}
