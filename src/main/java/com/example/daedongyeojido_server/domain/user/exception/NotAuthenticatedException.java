da4a54fe8988cfd77d96f95b1cae9b3d06434aa7
dkflfkd53 <b57062562@gmail.com>
2024-01-24 18:48:31 +0900

    🌧 :: 인증되지 않은 유저 예외 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/user/exception/NotAuthenticatedException.java
@@ -0,0 +1,8 @@
+package com.example.daedongyeojido_server.domain.user.exception;
+
+public class NotAuthenticatedException extends RuntimeException{
+
+    public NotAuthenticatedException(String message) {
+        super(message);
+    }
+}
