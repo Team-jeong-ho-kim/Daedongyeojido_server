d13b644c0c4c9e9540c8b4c8541f287887f02a15
김가은 <kge02160@gmail.com>
2024-01-24 21:52:55 +0900

    🌧 :: modifyClubService 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/club/application/ModifyClubService.java
@@ -0,0 +1,30 @@
+package com.example.daedongyeojido_server.domain.club.application;
+
+import com.example.daedongyeojido_server.domain.club.dao.ClubRepository;
+import com.example.daedongyeojido_server.domain.club.domain.Club;
+import com.example.daedongyeojido_server.domain.club.dto.request.UpdateClubRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class ModifyClubService {
+
+    private final ClubRepository clubRepository;
+
+    @Transactional
+    public void modifyClub(String clubName, UpdateClubRequest request) {
+        Club club = clubRepository.findById(clubName)
+                .orElseThrow(RuntimeException::new);
+
+        club.modifyClub(
+                request.getClubName(),
+                request.getTitle(),
+                request.getContent(),
+                request.getClubImageUrl(),
+                request.getClubMembers(),
+                request.getTags(),
+                request.getNotices());
+    }
+}
