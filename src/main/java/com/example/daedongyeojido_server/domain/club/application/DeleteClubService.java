a92f9c0fef30eeef43623b36f31d723f9e5f6b7b
김가은 <kge02160@gmail.com>
2024-01-24 21:51:30 +0900

    🌧 :: deleteClubService 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/club/application/DeleteClubService.java
@@ -0,0 +1,20 @@
+package com.example.daedongyeojido_server.domain.club.application;
+
+import com.example.daedongyeojido_server.domain.club.dao.ClubRepository;
+import com.example.daedongyeojido_server.domain.club.domain.Club;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class DeleteClubService {
+
+    private final ClubRepository clubRepository;
+    @Transactional
+    public void deleteClub(String clubName) {
+        Club club = clubRepository.findById(clubName)
+                .orElseThrow(RuntimeException::new);
+        clubRepository.delete(club);
+    }
+}
