268c348a058a6d04fc57ca08b6f00232b43aba6c
김가은 <kge02160@gmail.com>
2024-01-24 16:38:33 +0900

    🌧 :: ClubRepository clubName 추가

--- src/main/java/com/example/daedongyeojido_server/domain/club/dao/ClubRepository.java
+++ src/main/java/com/example/daedongyeojido_server/domain/club/dao/ClubRepository.java
@@ -4,4 +4,6 @@ import com.example.daedongyeojido_server.domain.club.domain.Club;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClubRepository extends JpaRepository<Club, String> {
+
+    Club findByClubName(String clubName);
 }
