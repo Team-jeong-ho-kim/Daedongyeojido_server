24e910a07b7e48e2f007361ea6063ddefbf6e23f
dkflfkd53 <b57062562@gmail.com>
2024-01-24 19:04:32 +0900

    ♻️ :: 마이페이지 service 수정

--- src/main/java/com/example/daedongyeojido_server/domain/user/application/MyInfoService.java
+++ src/main/java/com/example/daedongyeojido_server/domain/user/application/MyInfoService.java
@@ -1,6 +1,7 @@
 package com.example.daedongyeojido_server.domain.user.application;
 
 import com.example.daedongyeojido_server.domain.user.domain.User;
+import com.example.daedongyeojido_server.domain.user.dto.response.MyInfoResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,7 +11,14 @@ public class MyInfoService {
 
     private final UserFacade userFacade;
 
-    public User myInfo() {
-        return userFacade.currentTeacher();
+    public MyInfoResponse myInfo() {
+        User user = userFacade.currentTeacher();
+
+        return MyInfoResponse.builder()
+                .classNumber(user.getClassNumber())
+                .name(user.getName())
+                .part(user.getPart())
+                .myClub(user.getMyClub())
+                .build();
     }
 }
