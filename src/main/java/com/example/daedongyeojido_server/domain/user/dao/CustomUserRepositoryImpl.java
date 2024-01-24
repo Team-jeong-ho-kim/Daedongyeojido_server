47a33e97baed5262f8d253a116233d9e1829dd00
dkflfkd53 <b57062562@gmail.com>
2024-01-24 16:15:39 +0900

    🌧 :: CustomUserRepositoryImpl 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/user/dao/CustomUserRepositoryImpl.java
@@ -0,0 +1,29 @@
+package com.example.daedongyeojido_server.domain.user.dao;
+
+import com.example.daedongyeojido_server.domain.user.domain.User;
+import com.example.daedongyeojido_server.domain.user.domain.enums.Part;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+import static com.example.daedongyeojido_server.domain.user.domain.QUser.user;
+
+@Repository
+@RequiredArgsConstructor
+public class CustomUserRepositoryImpl implements CustomUserRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    public Optional<User> findTeacherByName(String name) {
+        User teacher = queryFactory
+                .selectFrom(user)
+                .where(user.part.eq(Part.TEACHER)
+                        .or(user.part.eq(Part.CLUB_LEADER_TEACHER)))
+                .where(user.name.eq(name))
+                .fetchOne();
+
+        return Optional.ofNullable(teacher);
+    }
+}
