4e231b02e758e5fc4adacb483b693a5812f2a129
dkflfkd53 <b57062562@gmail.com>
2024-01-24 16:16:13 +0900

    ♻️ :: findTeacherByName 메서드로 변경

--- src/main/java/com/example/daedongyeojido_server/domain/auth/application/CheckUserService.java
+++ src/main/java/com/example/daedongyeojido_server/domain/auth/application/CheckUserService.java
@@ -1,6 +1,7 @@
 package com.example.daedongyeojido_server.domain.auth.application;
 
 import com.example.daedongyeojido_server.domain.auth.dto.request.SignupAndCheckUserRequest;
+import com.example.daedongyeojido_server.domain.user.dao.CustomUserRepository;
 import com.example.daedongyeojido_server.domain.user.dao.UserRepository;
 import com.example.daedongyeojido_server.domain.user.domain.enums.Part;
 import lombok.RequiredArgsConstructor;
@@ -12,12 +13,14 @@ public class CheckUserService {
 
     private final UserRepository userRepository;
 
+    private final CustomUserRepository customUserRepository;
+
     public boolean checkUser(SignupAndCheckUserRequest request) {
         if (request.getPart() == Part.INDEPENDENT) {
             return userRepository.findByClassNumber(request.getClassNumber()).isPresent();
         }
         else {
-            return userRepository.findByName(request.getName()).isPresent();
+            return customUserRepository.findTeacherByName(request.getName()).isPresent();
         }
     }
 }
--- src/main/java/com/example/daedongyeojido_server/domain/auth/application/TeacherLoginService.java
+++ src/main/java/com/example/daedongyeojido_server/domain/auth/application/TeacherLoginService.java
@@ -2,7 +2,7 @@ package com.example.daedongyeojido_server.domain.auth.application;
 
 import com.example.daedongyeojido_server.domain.auth.dto.request.TeacherLoginRequest;
 import com.example.daedongyeojido_server.domain.auth.dto.response.TokenResponse;
-import com.example.daedongyeojido_server.domain.user.dao.UserRepository;
+import com.example.daedongyeojido_server.domain.user.dao.CustomUserRepository;
 import com.example.daedongyeojido_server.domain.user.domain.User;
 import com.example.daedongyeojido_server.domain.user.exception.UserNotFoundException;
 import com.example.daedongyeojido_server.global.security.jwt.JwtTokenProvider;
@@ -13,13 +13,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class TeacherLoginService {
 
-    private final UserRepository userRepository;
+    private final CustomUserRepository customUserRepository;
 
     private final JwtTokenProvider jwtTokenProvider;
 
     public TokenResponse teacherLogin(TeacherLoginRequest request) {
-        User user = userRepository.findByName(request.getName())
-                .orElseThrow(()-> UserNotFoundException.EXCEPTION);
+        User user = customUserRepository.findTeacherByName(request.getName())
+                .orElseThrow(()->UserNotFoundException.EXCEPTION);
 
         return jwtTokenProvider.receiveToken(request.getName());
     }
