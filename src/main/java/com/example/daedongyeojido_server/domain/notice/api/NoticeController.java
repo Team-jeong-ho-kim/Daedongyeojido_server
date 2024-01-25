a08adbb3e339eb966031d55db7f49448cd77e56c
dkflfkd53 <b57062562@gmail.com>
2024-01-26 06:24:24 +0900

    🌧 :: 공고 삭제 api service 추가

--- src/main/java/com/example/daedongyeojido_server/domain/notice/api/NoticeController.java
+++ src/main/java/com/example/daedongyeojido_server/domain/notice/api/NoticeController.java
@@ -1,9 +1,12 @@
 package com.example.daedongyeojido_server.domain.notice.api;
 
 import com.example.daedongyeojido_server.domain.notice.application.CreateNoticeService;
+import com.example.daedongyeojido_server.domain.notice.application.DeleteNoticeService;
 import com.example.daedongyeojido_server.domain.notice.dto.request.CreateNoticeRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +19,15 @@ public class NoticeController {
 
     private final CreateNoticeService createNoticeService;
 
+    private final DeleteNoticeService deleteNoticeService;
+
     @PostMapping("/create")
     public void createNotice(@RequestBody @Valid CreateNoticeRequest request) {
         createNoticeService.createNotice(request);
+    }
+
+    @DeleteMapping("/delete/{noticeId}")
+    public void deleteNotice(@PathVariable Long noticeId) {
+        deleteNoticeService.deleteNotice(noticeId);
     }
 }
