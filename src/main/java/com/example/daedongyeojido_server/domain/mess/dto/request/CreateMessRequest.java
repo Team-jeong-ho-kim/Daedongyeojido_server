6020a16a1a67c12657882febcb11717e4c694e55
김가은 <kge02160@gmail.com>
2024-01-25 14:42:16 +0900

    🌧 :: CreateMessRepository 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/mess/dto/request/CreateMessRequest.java
@@ -0,0 +1,31 @@
+package com.example.daedongyeojido_server.domain.mess.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CreateMessRequest {
+
+    @NotBlank
+    private Long messId;
+
+    @NotBlank
+    @Size(max = 2)
+    private Integer messStartTime;
+
+    @NotBlank
+    @Size(max = 2)
+    private Integer messEndTime;
+
+    @NotBlank
+    private Boolean messAgree;
+
+    @NotBlank
+    private LocalDate messDate;
+}
