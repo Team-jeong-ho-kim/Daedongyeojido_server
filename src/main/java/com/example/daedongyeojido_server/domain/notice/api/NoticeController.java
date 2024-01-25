669927cb0c3eb384255fb60c4cdfa5b81069ec7c
dkflfkd53 <b57062562@gmail.com>
2024-01-26 06:35:10 +0900

    🌧 :: 공고 조회 api controller 추가

--- src/main/java/com/example/daedongyeojido_server/domain/notice/api/NoticeController.java
+++ src/main/java/com/example/daedongyeojido_server/domain/notice/api/NoticeController.java
@@ -2,15 +2,20 @@ package com.example.daedongyeojido_server.domain.notice.api;
 
 import com.example.daedongyeojido_server.domain.notice.application.CreateNoticeService;
 import com.example.daedongyeojido_server.domain.notice.application.DeleteNoticeService;
+import com.example.daedongyeojido_server.domain.notice.application.QueryNoticeService;
 import com.example.daedongyeojido_server.domain.notice.dto.request.CreateNoticeRequest;
+import com.example.daedongyeojido_server.domain.notice.dto.response.NoticeResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,11 +24,18 @@ public class NoticeController {
 
     private final CreateNoticeService createNoticeService;
 
+    private final QueryNoticeService queryNoticeService;
+
     private final DeleteNoticeService deleteNoticeService;
 
     @PostMapping("/create")
     public void createNotice(@RequestBody @Valid CreateNoticeRequest request) {
         createNoticeService.createNotice(request);
+    }
+
+    @GetMapping("/{clubName}")
+    public List<NoticeResponse> queryNotice(@PathVariable String clubName) {
+        return queryNoticeService.queryNotice(clubName);
     }
 
     @DeleteMapping("/delete/{noticeId}")
