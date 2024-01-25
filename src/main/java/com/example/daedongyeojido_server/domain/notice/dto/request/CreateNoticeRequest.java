e3d593cd8ac67aa863b15ad79620d3e82bb1d3da
dkflfkd53 <b57062562@gmail.com>
2024-01-26 06:18:16 +0900

    🌧 :: 공고 생성 api reqeust추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/notice/dto/request/CreateNoticeRequest.java
@@ -0,0 +1,24 @@
+package com.example.daedongyeojido_server.domain.notice.dto.request;
+
+import com.example.daedongyeojido_server.domain.notice.domain.enums.Major;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CreateNoticeRequest {
+
+    private Major major;
+
+    @NotBlank
+    @Size(min = 1, max = 20, message = "clubName은 1글자 이상, 20글자 이하여야 합니다.")
+    private String clubName;
+
+    @NotBlank
+    private LocalDateTime deadline;
+}
