aaff6fe422ab6f8054224de3badd938e1fdc4d3b
dkflfkd53 <b57062562@gmail.com>
2024-01-29 20:44:37 +0900

    🌧 :: 공고 조회 api service 추가

--- src/main/java/com/example/daedongyeojido_server/domain/notice/application/QueryNoticeService.java
+++ src/main/java/com/example/daedongyeojido_server/domain/notice/application/QueryNoticeService.java
@@ -3,9 +3,14 @@ package com.example.daedongyeojido_server.domain.notice.application;
 import com.example.daedongyeojido_server.domain.club.dao.ClubRepository;
 import com.example.daedongyeojido_server.domain.club.domain.Club;
 import com.example.daedongyeojido_server.domain.notice.dao.NoticeRepository;
+import com.example.daedongyeojido_server.domain.notice.domain.Notice;
 import com.example.daedongyeojido_server.domain.notice.dto.response.NoticeResponse;
+import com.example.daedongyeojido_server.domain.report.dao.ReportRepository;
+import com.example.daedongyeojido_server.domain.user.application.UserFacade;
+import com.example.daedongyeojido_server.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,16 +19,35 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class QueryNoticeService {
 
+    private final UserFacade userFacade;
+
     private final NoticeRepository noticeRepository;
 
     private final ClubRepository clubRepository;
 
+    private final ReportRepository reportRepository;
+
+    @Transactional
     public List<NoticeResponse> queryNotice(String clubName) {
+        User user = userFacade.currentStudent();
+
         Club club = clubRepository.findByClubName(clubName);
 
-        return noticeRepository.findAllByClubName(club)
+        List<Notice> notices = noticeRepository.findAllByClubName(club);
+
+        List<NoticeResponse> noticeResponses = noticeRepository.findAllByClubName(club)
                 .stream()
                 .map(NoticeResponse::new)
                 .collect(Collectors.toList());
+
+        for(int i=0; i<noticeResponses.size(); i++) {
+            NoticeResponse noticeResponse = noticeResponses.get(i);
+            Notice notice = notices.get(i);
+            if(!(reportRepository.findAllByClassNumberAndNotice(user.getClassNumber(), notice).isEmpty())) {
+                noticeResponse.apply();
+            }
+        }
+
+        return noticeResponses;
     }
 }
