0a2e16873208d65691c839cf7b37f9cabd90230d
dkflfkd53 <b57062562@gmail.com>
2024-01-29 20:44:49 +0900

    🌧 :: 지원서 레포지토리 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/report/dao/ReportRepository.java
@@ -0,0 +1,11 @@
+package com.example.daedongyeojido_server.domain.report.dao;
+
+import com.example.daedongyeojido_server.domain.notice.domain.Notice;
+import com.example.daedongyeojido_server.domain.report.domain.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ReportRepository extends JpaRepository<Report, Long> {
+    List<Report> findAllByClassNumberAndNotice(String classNumber, Notice notice);
+}
