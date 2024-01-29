9292aeec7f54dde88f58828178e487f0a7243fbc
김가은 <kge02160@gmail.com>
2024-01-29 16:01:57 +0900

    ♻️ :: club info response 내용값 추가

--- src/main/java/com/example/daedongyeojido_server/domain/club/dto/response/ClubInfoResponse.java
+++ src/main/java/com/example/daedongyeojido_server/domain/club/dto/response/ClubInfoResponse.java
@@ -1,8 +1,11 @@
 package com.example.daedongyeojido_server.domain.club.dto.response;
 
+import com.example.daedongyeojido_server.domain.user.domain.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 @Builder
@@ -10,4 +13,14 @@ import lombok.Getter;
 public class ClubInfoResponse {
 
     private String clubName;
+
+    private String title;
+
+    private String contents;
+
+    private String clubImageUrl;
+
+    private List<String> tags;
+
+    private List<User> clubMembers;
 }
