f8686625cac1272347f1dd9eff64280f925d8278
김가은 <kge02160@gmail.com>
2024-01-29 15:06:41 +0900

    🌧 ::  CreateAnnouncementRequest 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/alarm/dto/request/CreateAnnouncementRequest.java
@@ -0,0 +1,22 @@
+package com.example.daedongyeojido_server.domain.alarm.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CreateAnnouncementRequest {
+
+    @NotBlank
+    private Long alarmId;
+
+    @NotBlank
+    @Size(min = 1, max = 20, message = "제목은 1글자 이상, 20글자 이하여야 합니다, 또한 작성하지 않으면 안됩니다.")
+    private String title;
+
+    @Size(min = 1, max = 200, message = "내용은 1글자 이상, 200글자 이하여야 합니다.")
+    private String contents;
+}
