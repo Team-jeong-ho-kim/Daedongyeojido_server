12a9ff6093943b173902041e725cd320a21f1e18
김가은 <kge02160@gmail.com>
2024-01-30 13:27:04 +0900

    ♻️ :: 어노테이션 추가

--- src/main/java/com/example/daedongyeojido_server/domain/club/application/QueryAllClubService.java
+++ src/main/java/com/example/daedongyeojido_server/domain/club/application/QueryAllClubService.java
@@ -4,6 +4,7 @@ import com.example.daedongyeojido_server.domain.club.dao.ClubRepository;
 import com.example.daedongyeojido_server.domain.club.domain.Club;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ public class QueryAllClubService {
 
     private final ClubRepository clubRepository;
 
+    @Transactional
     public List<Club> queryAllClub() {
         return clubRepository.findAll();
     }
