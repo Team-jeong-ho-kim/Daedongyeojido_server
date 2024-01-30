01132554dc2cfab602dd1dc0ae8e2abb05a8aa23
김가은 <kge02160@gmail.com>
2024-01-30 13:26:04 +0900

    ♻️ :: 이름 변경, 최소 글자 수 추가

--- src/main/java/com/example/daedongyeojido_server/domain/club/dto/request/ModifyClubRequest.java
+++ src/main/java/com/example/daedongyeojido_server/domain/club/dto/request/ModifyClubRequest.java
@@ -1,6 +1,5 @@
 package com.example.daedongyeojido_server.domain.club.dto.request;
 
-import com.example.daedongyeojido_server.domain.notice.domain.Notice;
 import com.example.daedongyeojido_server.domain.user.domain.User;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -12,16 +11,16 @@ import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class UpdateClubRequest {
+public class ModifyClubRequest {
 
     @NotBlank
     @Size(min = 1, max = 20, message = "clubname은 1글자 이상, 20글자 이하여야 합니다.")
     private String clubName;
 
-    @Size(max = 30, message = "제목은 30글자 이하로 작성하여야 합니다.")
+    @Size(min = 1, max = 30, message = "제목은 30글자 이하로 작성하여야 합니다.")
     private String title;
 
-    @Size(max = 300, message = "소개 글은 300글자 이하로 작성하여야 합니다.")
+    @Size(min = 1, max = 300, message = "소개 글은 300글자 이하로 작성하여야 합니다.")
     private String content;
 
     private String clubImageUrl;
