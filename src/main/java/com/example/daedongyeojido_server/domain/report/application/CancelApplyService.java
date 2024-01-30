ab14f4017e52775a56f4b83cbfe6344eab49525a
dkflfkd53 <b57062562@gmail.com>
2024-01-30 18:21:47 +0900

    🌧 :: 지원 취소 api service 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/report/application/CancelApplyService.java
@@ -0,0 +1,18 @@
+package com.example.daedongyeojido_server.domain.report.application;
+
+import com.example.daedongyeojido_server.domain.report.dao.ReportRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CancelApplyService {
+
+    private final ReportRepository reportRepository;
+
+    @Transactional
+    public void cancelApply(Long applyId) {
+        reportRepository.deleteById(applyId);
+    }
+}
