e96a2b76ef46018ce43ce8ee3d8573ddf7064307
dkflfkd53 <b57062562@gmail.com>
2024-01-30 18:22:10 +0900

    🌧 :: 지원서 response 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/report/dto/response/ReportResponse.java
@@ -0,0 +1,23 @@
+package com.example.daedongyeojido_server.domain.report.dto.response;
+
+import com.example.daedongyeojido_server.domain.notice.domain.enums.Major;
+import com.example.daedongyeojido_server.domain.report.domain.Report;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ReportResponse {
+
+    private String classNumber;
+
+    private String name;
+
+    private Major hopeMajor;
+
+    public ReportResponse(Report report) {
+        classNumber = report.getClassNumber();
+        name = report.getName();
+        hopeMajor = report.getNotice().getMajor();
+    }
+}
