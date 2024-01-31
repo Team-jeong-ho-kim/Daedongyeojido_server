b9e66eaaf1155be0940a8af5bc81526685129fd1
dkflfkd53 <b57062562@gmail.com>
2024-01-31 14:23:28 +0900

    🌧 :: findMyReport 메서드 추가

--- src/main/java/com/example/daedongyeojido_server/domain/report/dao/CustomReportRepository.java
+++ src/main/java/com/example/daedongyeojido_server/domain/report/dao/CustomReportRepository.java
@@ -2,9 +2,12 @@ package com.example.daedongyeojido_server.domain.report.dao;
 
 import com.example.daedongyeojido_server.domain.club.domain.Club;
 import com.example.daedongyeojido_server.domain.report.domain.Report;
+import com.example.daedongyeojido_server.domain.user.domain.User;
 
 import java.util.List;
 
 public interface CustomReportRepository {
     List<Report> findClubReport(Club club);
+
+    List<Report> findMyReport(User user);
 }
