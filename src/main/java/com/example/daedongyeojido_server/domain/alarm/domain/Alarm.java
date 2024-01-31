17728e343905a309e3cebe6a7c8469344bd465c5
dkflfkd53 <b57062562@gmail.com>
2024-01-31 21:27:00 +0900

    ♻️ :: 알람 entity 수정

--- src/main/java/com/example/daedongyeojido_server/domain/alarm/domain/Alarm.java
+++ src/main/java/com/example/daedongyeojido_server/domain/alarm/domain/Alarm.java
@@ -1,8 +1,24 @@
 package com.example.daedongyeojido_server.domain.alarm.domain;
 
-import com.example.daedongyeojido_server.domain.alarm.domain.enums.PassOrNot;
-import jakarta.persistence.*;
-import lombok.*;
+import com.example.daedongyeojido_server.domain.alarm.domain.enums.AlarmType;
+import com.example.daedongyeojido_server.domain.notice.domain.enums.Major;
+import com.example.daedongyeojido_server.domain.report.domain.enums.PassingResult;
+import com.example.daedongyeojido_server.domain.user.domain.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
@@ -15,19 +31,31 @@ public class Alarm {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long alarmId;
 
-    @Column(name = "title", length = 20, nullable = false)
+    @Column(name = "title", length = 20)
     private String title;
 
     @Column(name = "contents", length = 200)
     private String contents;
-
-    @Column(name = "pass_or_not")
-    @Enumerated(EnumType.STRING)
-    private PassOrNot passOrNot;
 
     @Column(name = "club_name", length = 20)
     private String clubName;
 
     @Column(name = "user_name", length = 4)
     private String userName;
+
+    @Column(name = "passing_result")
+    @Enumerated(EnumType.STRING)
+    private PassingResult passingResult;
+
+    @Column(name = "major")
+    @Enumerated(EnumType.STRING)
+    private Major major;
+
+    @Column(name = "alarm_type")
+    @Enumerated(EnumType.STRING)
+    private AlarmType alarmType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
