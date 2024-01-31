11bd823a93088a898e4991bd55f65541bc22330c
dkflfkd53 <b57062562@gmail.com>
2024-01-31 22:16:45 +0900

    🌧 :: 알람 조회 api service 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/alarm/application/MyAlarmService.java
@@ -0,0 +1,31 @@
+package com.example.daedongyeojido_server.domain.alarm.application;
+
+import com.example.daedongyeojido_server.domain.alarm.dao.AlarmRepository;
+import com.example.daedongyeojido_server.domain.alarm.dto.response.AlarmResponse;
+import com.example.daedongyeojido_server.domain.user.application.UserFacade;
+import com.example.daedongyeojido_server.domain.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class MyAlarmService {
+
+    private final AlarmRepository alarmRepository;
+
+    private final UserFacade userFacade;
+
+    @Transactional
+    public List<AlarmResponse> myAlarm() {
+        User user = userFacade.currentStudent();
+
+        return alarmRepository.findAllByUser(user)
+                .stream()
+                .map(AlarmResponse::new)
+                .collect(Collectors.toList());
+    }
+}
