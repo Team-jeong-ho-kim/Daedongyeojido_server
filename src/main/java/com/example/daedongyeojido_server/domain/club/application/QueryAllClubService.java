05974b5d125cfd19b15b1510ad2b989d4dc85532
김가은 <kge02160@gmail.com>
2024-01-31 14:44:48 +0900

    🌧 :: QueryAllClubService 추가

--- src/main/java/com/example/daedongyeojido_server/domain/club/application/QueryAllClubService.java
+++ src/main/java/com/example/daedongyeojido_server/domain/club/application/QueryAllClubService.java
@@ -1,12 +1,13 @@
 package com.example.daedongyeojido_server.domain.club.application;
 
 import com.example.daedongyeojido_server.domain.club.dao.ClubRepository;
-import com.example.daedongyeojido_server.domain.club.domain.Club;
+import com.example.daedongyeojido_server.domain.club.dto.response.QueryAllClubResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,7 +16,11 @@ public class QueryAllClubService {
     private final ClubRepository clubRepository;
 
     @Transactional
-    public List<Club> queryAllClub() {
-        return clubRepository.findAll();
+    public List<QueryAllClubResponse> queryAllClub() {
+
+        return clubRepository.findAll()
+                .stream()
+                .map(QueryAllClubResponse::new)
+                .collect(Collectors.toList());
     }
 }
