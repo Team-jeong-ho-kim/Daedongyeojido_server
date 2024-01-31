9bdf75e9eb7b6c5d1a1a6faa109c05cc4b925f9f
dkflfkd53 <b57062562@gmail.com>
2024-01-31 13:41:25 +0900

    🌧 :: 지원서 상세조회 api service 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/report/application/ReportInfoService.java
@@ -0,0 +1,31 @@
+package com.example.daedongyeojido_server.domain.report.application;
+
+import com.example.daedongyeojido_server.domain.report.dao.ReportRepository;
+import com.example.daedongyeojido_server.domain.report.domain.Report;
+import com.example.daedongyeojido_server.domain.report.dto.response.ReportInfoResponse;
+import com.example.daedongyeojido_server.domain.report.exception.ReportNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ReportInfoService {
+
+    private final ReportRepository reportRepository;
+
+    @Transactional
+    public ReportInfoResponse reportInfo(Long reportId) {
+        Report report = reportRepository.findById(reportId)
+                .orElseThrow(()-> ReportNotFoundException.EXCEPTION);
+
+        return ReportInfoResponse.builder()
+                .classNumber(report.getClassNumber())
+                .name(report.getName())
+                .oneLiner(report.getOneLiner())
+                .introduction(report.getIntroduction())
+                .hopeMajor(report.getHopeMajor())
+                .learn(report.getLearn())
+                .build();
+    }
+}
