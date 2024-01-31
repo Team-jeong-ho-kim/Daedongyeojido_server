66aa2ce6444b0d412de976e4ca5aabd219ff1c2e
dkflfkd53 <b57062562@gmail.com>
2024-01-31 21:24:02 +0900

    🌧 :: 면접 결과 api controller 추가

--- src/main/java/com/example/daedongyeojido_server/domain/alarm/api/AlarmController.java
+++ src/main/java/com/example/daedongyeojido_server/domain/alarm/api/AlarmController.java
@@ -1,7 +1,9 @@
 package com.example.daedongyeojido_server.domain.alarm.api;
 
 import com.example.daedongyeojido_server.domain.alarm.application.CreateAnnouncementService;
+import com.example.daedongyeojido_server.domain.alarm.application.InterviewResultService;
 import com.example.daedongyeojido_server.domain.alarm.dto.request.CreateAnnouncementRequest;
+import com.example.daedongyeojido_server.domain.alarm.dto.request.InterviewResultRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,9 +16,16 @@ public class AlarmController {
 
     private final CreateAnnouncementService createAnnouncementService;
 
+    private final InterviewResultService interviewResultService;
+
     @ResponseStatus(HttpStatus.OK)
     @PostMapping("/announcement")
     public void createAnnouncement(@RequestBody @Valid CreateAnnouncementRequest request){
         createAnnouncementService.createAnnouncement(request);
+    }
+
+    @PostMapping("/result")
+    public void interviewResult(@RequestBody @Valid InterviewResultRequest request) {
+        interviewResultService.interviewResult(request);
     }
 }
