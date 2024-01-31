0fb14d50bef016608e3ebf6dbb22b0a2719cc2bc
dkflfkd53 <b57062562@gmail.com>
2024-01-31 22:17:01 +0900

    🌧 :: findAllByUser 메서드 추가

--- src/main/java/com/example/daedongyeojido_server/domain/alarm/dao/AlarmRepository.java
+++ src/main/java/com/example/daedongyeojido_server/domain/alarm/dao/AlarmRepository.java
@@ -1,7 +1,11 @@
 package com.example.daedongyeojido_server.domain.alarm.dao;
 
 import com.example.daedongyeojido_server.domain.alarm.domain.Alarm;
+import com.example.daedongyeojido_server.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
+    List<Alarm> findAllByUser(User user);
 }
