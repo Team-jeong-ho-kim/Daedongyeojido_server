42bc7ab80927762eb4817b7e2212ef9d00ccea35
dkflfkd53 <b57062562@gmail.com>
2024-01-31 13:40:12 +0900

    🌧 :: ReportNotFound 예외 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/report/exception/ReportNotFoundException.java
@@ -0,0 +1,14 @@
+package com.example.daedongyeojido_server.domain.report.exception;
+
+import com.example.daedongyeojido_server.global.error.exception.BusinessException;
+import com.example.daedongyeojido_server.global.error.exception.ErrorCode;
+
+public class ReportNotFoundException extends BusinessException {
+
+    public static final BusinessException EXCEPTION = new ReportNotFoundException();
+
+    public ReportNotFoundException() {
+        super(ErrorCode.REPORT_NOT_FOUND);
+    }
+}
+
