98b43c4b5a42f27cbad8b36fe0760e35c17f797e
dkflfkd53 <b57062562@gmail.com>
2024-02-01 22:07:29 +0900

    ♻️ :: 공지 추가 api 수정

--- src/main/java/com/example/daedongyeojido_server/domain/alarm/application/CreateAnnouncementService.java
+++ src/main/java/com/example/daedongyeojido_server/domain/alarm/application/CreateAnnouncementService.java
@@ -4,6 +4,8 @@ import com.example.daedongyeojido_server.domain.alarm.dao.AlarmRepository;
 import com.example.daedongyeojido_server.domain.alarm.domain.Alarm;
 import com.example.daedongyeojido_server.domain.alarm.domain.enums.AlarmType;
 import com.example.daedongyeojido_server.domain.alarm.dto.request.CreateAnnouncementRequest;
+import com.example.daedongyeojido_server.domain.user.application.UserFacade;
+import com.example.daedongyeojido_server.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,13 +16,18 @@ public class CreateAnnouncementService {
 
     private final AlarmRepository alarmRepository;
 
+    private final UserFacade userFacade;
+
     @Transactional
     public void createAnnouncement(CreateAnnouncementRequest request) {
+        User user = userFacade.currentTeacher();
+
         alarmRepository.save(
                 Alarm.builder()
                         .title(request.getTitle())
                         .contents(request.getContents())
                         .alarmType(AlarmType.ANNOUNCEMENT)
+                        .user(user)
                         .build());
     }
 }
