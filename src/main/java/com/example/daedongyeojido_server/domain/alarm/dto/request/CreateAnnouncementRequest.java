7585a954cf2cb43c4a83f6f945cf9ed0622463dd
dkflfkd53 <b57062562@gmail.com>
2024-02-01 15:04:23 +0900

    ♻️ :: 공지사항 추가 api request 수정

--- src/main/java/com/example/daedongyeojido_server/domain/alarm/dto/request/CreateAnnouncementRequest.java
+++ src/main/java/com/example/daedongyeojido_server/domain/alarm/dto/request/CreateAnnouncementRequest.java
@@ -1,6 +1,7 @@
 package com.example.daedongyeojido_server.domain.alarm.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -10,13 +11,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CreateAnnouncementRequest {
 
-    @NotBlank
+    @NotNull
     private Long alarmId;
 
     @NotBlank
     @Size(min = 1, max = 20, message = "제목은 1글자 이상, 20글자 이하여야 합니다, 또한 작성하지 않으면 안됩니다.")
     private String title;
 
+    @NotBlank
     @Size(min = 1, max = 200, message = "내용은 1글자 이상, 200글자 이하여야 합니다.")
     private String contents;
 }
