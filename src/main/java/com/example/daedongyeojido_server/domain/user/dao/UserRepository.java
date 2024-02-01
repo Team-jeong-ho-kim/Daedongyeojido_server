5e020c9a4bb5fd41939c622c0265ad824e85554a
김가은 <kge02160@gmail.com>
2024-02-01 23:46:44 +0900

    ♻️ :: findAllByMyClub 추가

--- src/main/java/com/example/daedongyeojido_server/domain/user/dao/UserRepository.java
+++ src/main/java/com/example/daedongyeojido_server/domain/user/dao/UserRepository.java
@@ -1,11 +1,15 @@
 package com.example.daedongyeojido_server.domain.user.dao;
 
+import com.example.daedongyeojido_server.domain.club.domain.Club;
 import com.example.daedongyeojido_server.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByClassNumber(String classNumber);
+
+    List<User> findAllByMyClub(Club myClub);
 }
