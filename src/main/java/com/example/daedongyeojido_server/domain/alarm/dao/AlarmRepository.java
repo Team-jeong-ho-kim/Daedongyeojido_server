7330f8c5239aa77db8903e22987062f43535e2b6
dkflfkd53 <b57062562@gmail.com>
2024-02-01 22:13:29 +0900

    ♻️ :: findAllByAlarmType 메서드 추가

--- src/main/java/com/example/daedongyeojido_server/domain/alarm/dao/AlarmRepository.java
+++ src/main/java/com/example/daedongyeojido_server/domain/alarm/dao/AlarmRepository.java
@@ -1,6 +1,7 @@
 package com.example.daedongyeojido_server.domain.alarm.dao;
 
 import com.example.daedongyeojido_server.domain.alarm.domain.Alarm;
+import com.example.daedongyeojido_server.domain.alarm.domain.enums.AlarmType;
 import com.example.daedongyeojido_server.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,6 @@ import java.util.List;
 
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
     List<Alarm> findAllByUser(User user);
+
+    List<Alarm> findAllByAlarmType(AlarmType alarmType);
 }
