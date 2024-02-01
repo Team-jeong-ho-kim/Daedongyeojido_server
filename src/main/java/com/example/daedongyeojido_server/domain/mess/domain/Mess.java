eeaa71913587b3819a373bacd75b90054e3cf75a
dkflfkd53 <b57062562@gmail.com>
2024-02-01 16:12:12 +0900

    🌧 :: 회식 entity에 회식 수락관련 메서드, 컬럼 추가

--- src/main/java/com/example/daedongyeojido_server/domain/mess/domain/Mess.java
+++ src/main/java/com/example/daedongyeojido_server/domain/mess/domain/Mess.java
@@ -3,6 +3,7 @@ package com.example.daedongyeojido_server.domain.mess.domain;
 import com.example.daedongyeojido_server.domain.club.domain.Club;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import java.time.LocalDate;
 @Entity
 @Getter
 @Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Mess {
 
@@ -28,6 +30,9 @@ public class Mess {
     @Column(name = "mess_date", nullable = false)
     private LocalDate messDate;
 
+    @Column(name = "math_accept")
+    private Integer MessAccept = 0;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "my_club")
     private Club myclub;
@@ -39,5 +44,9 @@ public class Mess {
         this.messEndTime = messEndTime;
         this.messDate = messDate;
         this.myclub = myclub;
+    }
+
+    public void acceptOrCancelMess(int i) {
+        this.MessAccept += i;
     }
 }
