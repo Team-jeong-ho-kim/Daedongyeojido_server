59386e9c84312c97a890a2b70e90d444b55f95ca
dkflfkd53 <b57062562@gmail.com>
2024-02-01 15:13:06 +0900

    ♻️ :: 동아리원 추가 api request 수정

--- src/main/java/com/example/daedongyeojido_server/domain/user/dto/request/AddClubMemberRequest.java
+++ src/main/java/com/example/daedongyeojido_server/domain/user/dto/request/AddClubMemberRequest.java
@@ -13,17 +13,17 @@ import lombok.NoArgsConstructor;
 public class AddClubMemberRequest {
 
     @NotBlank
-    @Size(min = 4, max = 4, message = "classNumber는 4글자 이여야 합니다.")
+    @Size(min = 4, max = 4, message = "학번은 4글자 이어야 합니다.")
     private String classNumber;
 
     @NotBlank
-    @Size(min = 3, max = 4, message = "name은 3글자 이상, 4글자 이하여야 합니다.")
+    @Size(min = 3, max = 4, message = "이름은 3글자 이상, 4글자 이하여야 합니다.")
     private String name;
 
     @NotNull
     private Part part;
 
     @NotBlank
-    @Size(min = 1, max = 20, message = "clubname은 1글자 이상, 20글자 이하여야 합니다.")
+    @Size(min = 1, max = 20, message = "동아리 이름은 1글자 이상, 20글자 이하여야 합니다.")
     private String clubName;
 }
