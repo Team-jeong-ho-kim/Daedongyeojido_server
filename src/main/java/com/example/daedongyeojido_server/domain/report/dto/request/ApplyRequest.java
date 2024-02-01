8a93ae816fb731b61380944f94ca787dfefc8b8c
dkflfkd53 <b57062562@gmail.com>
2024-02-01 15:12:11 +0900

    ♻️ :: 동아리 지원하기 api request 수정

--- src/main/java/com/example/daedongyeojido_server/domain/report/dto/request/ApplyRequest.java
+++ src/main/java/com/example/daedongyeojido_server/domain/report/dto/request/ApplyRequest.java
@@ -12,19 +12,19 @@ import lombok.NoArgsConstructor;
 public class ApplyRequest {
 
     @NotBlank
-    @Size(max = 20, message = "oneLiner는 20글자 이하여야 합니다.")
+    @Size(max = 20, message = "한 줄 소개는 20글자 이하여야 합니다.")
     private String oneLiner;
 
     @NotBlank
-    @Size(max = 100, message = "introduction은 100글자 이하여야 합니다.")
+    @Size(max = 100, message = "자기소개는 100글자 이하여야 합니다.")
     private String introduction;
 
     @NotBlank
-    @Size(max = 100, message = "hopeMajor는 100글자 이하여야 합니다.")
+    @Size(max = 100, message = "희망하는 전공은 100글자 이하여야 합니다.")
     private String hopeMajor;
 
     @NotBlank
-    @Size(max = 100, message = "learn은 100글자 이하여야 합니다.")
+    @Size(max = 100, message = "배우고 싶은 것은 100글자 이하여야 합니다.")
     private String learn;
 
     @NotNull
