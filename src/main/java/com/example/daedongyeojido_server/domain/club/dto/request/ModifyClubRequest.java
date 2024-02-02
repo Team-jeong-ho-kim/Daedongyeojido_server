7ff847b747c2b149e42efc7f1a56ac644c9faf02
김가은 <kge02160@gmail.com>
2024-02-02 14:09:59 +0900

    ♻️ :: members 제거

--- src/main/java/com/example/daedongyeojido_server/domain/club/dto/request/ModifyClubRequest.java
+++ src/main/java/com/example/daedongyeojido_server/domain/club/dto/request/ModifyClubRequest.java
@@ -1,7 +1,5 @@
 package com.example.daedongyeojido_server.domain.club.dto.request;
 
-import com.example.daedongyeojido_server.domain.user.domain.User;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -13,10 +11,6 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ModifyClubRequest {
 
-    @NotBlank
-    @Size(min = 1, max = 20, message = "clubname은 1글자 이상, 20글자 이하여야 합니다.")
-    private String clubName;
-
     @Size(min = 1, max = 30, message = "제목은 30글자 이하로 작성하여야 합니다.")
     private String title;
 
@@ -24,8 +18,6 @@ public class ModifyClubRequest {
     private String content;
 
     private String clubImageUrl;
-
-    private List<User> clubMembers;
 
     private List<String> tags;
 }
