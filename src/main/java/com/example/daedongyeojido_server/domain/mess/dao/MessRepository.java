811248dcafe87af5b0b4ffd6343e72e6163db733
김가은 <kge02160@gmail.com>
2024-02-02 16:41:12 +0900

    ♻️ :: findByMyClub 추가

--- src/main/java/com/example/daedongyeojido_server/domain/mess/dao/MessRepository.java
+++ src/main/java/com/example/daedongyeojido_server/domain/mess/dao/MessRepository.java
@@ -1,7 +1,11 @@
 package com.example.daedongyeojido_server.domain.mess.dao;
 
+import com.example.daedongyeojido_server.domain.club.domain.Club;
 import com.example.daedongyeojido_server.domain.mess.domain.Mess;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MessRepository extends JpaRepository<Mess, Long> {
+    Optional<Mess> findByMyClub(Club myclub);
 }
