92a245e89712962d463864aa4db59071dfee43d5
김가은 <kge02160@gmail.com>
2024-02-02 16:39:53 +0900

    ♻️ :: 생성자 제거 및 글자수 제한 제거

--- src/main/java/com/example/daedongyeojido_server/domain/mess/domain/Mess.java
+++ src/main/java/com/example/daedongyeojido_server/domain/mess/domain/Mess.java
@@ -2,11 +2,7 @@ package com.example.daedongyeojido_server.domain.mess.domain;
 
 import com.example.daedongyeojido_server.domain.club.domain.Club;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDate;
 
@@ -19,12 +15,13 @@ public class Mess {
 
     @Id
     @Column(name = "mess_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long messId;
 
-    @Column(name = "mess_start_time", length = 2, nullable = false)
+    @Column(name = "mess_start_time", nullable = false)
     private Integer messStartTime;
 
-    @Column(name = "mess_end_time", length = 2, nullable = false)
+    @Column(name = "mess_end_time", nullable = false)
     private Integer messEndTime;
 
     @Column(name = "mess_date", nullable = false)
@@ -33,18 +30,9 @@ public class Mess {
     @Column(name = "math_accept")
     private Integer MessAccept = 0;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "my_club")
-    private Club myclub;
-
-    @Builder
-    public Mess(Long messId, Integer messStartTime, Integer messEndTime, LocalDate messDate, Club myclub) {
-        this.messId = messId;
-        this.messStartTime = messStartTime;
-        this.messEndTime = messEndTime;
-        this.messDate = messDate;
-        this.myclub = myclub;
-    }
+    private Club myClub;
 
     public void acceptOrCancelMess(int i) {
         this.MessAccept += i;
