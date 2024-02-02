076760633db19f0a0220e3ecf119a6b08eecf691
김가은 <kge02160@gmail.com>
2024-02-02 16:44:36 +0900

    ♻️ :: 코드 수정 및 공백줄 삭제

--- src/main/java/com/example/daedongyeojido_server/domain/mess/application/QueryMessService.java
+++ src/main/java/com/example/daedongyeojido_server/domain/mess/application/QueryMessService.java
@@ -1,11 +1,13 @@
 package com.example.daedongyeojido_server.domain.mess.application;
 
 import com.example.daedongyeojido_server.domain.mess.dao.MessRepository;
-import com.example.daedongyeojido_server.domain.mess.domain.Mess;
+import com.example.daedongyeojido_server.domain.mess.dto.response.QueryMessResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +15,12 @@ public class QueryMessService {
 
     private final MessRepository messRepository;
 
-    public List<Mess> queryMess() {
-        return messRepository.findAll();
+    @Transactional
+    public List<QueryMessResponse> queryMess() {
+
+        return messRepository.findAll()
+                .stream()
+                .map(QueryMessResponse::new)
+                .collect(Collectors.toList());
     }
 }
