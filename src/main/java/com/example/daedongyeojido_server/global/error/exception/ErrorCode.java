82b95d4b6676be0ac400ee054ec3185972b43f34
김가은 <kge02160@gmail.com>
2024-02-03 20:21:57 +0900

    ♻️ :: RefreshToken에러코드 추가

--- src/main/java/com/example/daedongyeojido_server/global/error/exception/ErrorCode.java
+++ src/main/java/com/example/daedongyeojido_server/global/error/exception/ErrorCode.java
@@ -10,8 +10,10 @@ import lombok.Getter;
 public enum ErrorCode {
 
     // jwt
-    EXPIRED_TOKEN(401 , "만료된 토큰입니다."),
+    EXPIRED_TOKEN(401, "만료된 토큰입니다."),
     INVALID_TOKEN(401, "검증되지 않은 토큰입니다."),
+    INVALID_REFRESH_TOKEN(401, "검증되지 않은 리프레쉬토큰입니다."),
+    REFRESH_TOKEN_NOT_FOUND(404, "일치하는 리프레쉬토큰이 존재하지 않습니다."),
 
     // user
     USER_NOT_FOUND(404, "일치하는 유저가 존재하지 않습니다."),
