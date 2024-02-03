0a613ea62417e8b390a70928198cb42ce48f2d37
dkflfkd53 <b57062562@gmail.com>
2024-02-03 23:17:20 +0900

    ♻️ :: 이미지 url 생성 api url 수정

--- src/main/java/com/example/daedongyeojido_server/global/s3/api/S3Controller.java
+++ src/main/java/com/example/daedongyeojido_server/global/s3/api/S3Controller.java
@@ -3,19 +3,22 @@ package com.example.daedongyeojido_server.global.s3.api;
 import com.example.daedongyeojido_server.global.s3.dto.response.ImageUrlResponse;
 import com.example.daedongyeojido_server.global.s3.service.CreateImageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/aster")
+@RequestMapping("/jung-ho")
 public class S3Controller {
 
     private final CreateImageService createImageService;
 
+    @ResponseStatus(HttpStatus.OK)
     @PostMapping("/image")
     public ImageUrlResponse createImage(@RequestPart(value = "image", required = false) MultipartFile multipartFiles) {
         return createImageService.createImage(multipartFiles);
