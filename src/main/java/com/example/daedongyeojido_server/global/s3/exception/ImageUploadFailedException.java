2ea35f60fb970ff5776a067fd7b158a3899d7aa4
dkflfkd53 <b57062562@gmail.com>
2024-02-03 22:31:09 +0900

    🌧 :: 이미지 업로드 실패 예외 추가

---
+++ src/main/java/com/example/daedongyeojido_server/global/s3/exception/ImageUploadFailedException.java
@@ -0,0 +1,12 @@
+package com.example.daedongyeojido_server.global.s3.exception;
+
+import com.example.daedongyeojido_server.global.error.exception.BusinessException;
+import com.example.daedongyeojido_server.global.error.exception.ErrorCode;
+
+public class ImageUploadFailedException extends BusinessException {
+
+    public static final BusinessException EXCEPTION =
+            new ImageUploadFailedException();
+
+    private ImageUploadFailedException() { super(ErrorCode.IMAGE_UPLOAD_FAIL);}
+}
