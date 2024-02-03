10effa10266c8e45f9adf14999abd5b920936b77
dkflfkd53 <b57062562@gmail.com>
2024-02-03 22:31:55 +0900

    🌧 :: 이미지 url response 추가

---
+++ src/main/java/com/example/daedongyeojido_server/global/s3/dto/response/ImageUrlResponse.java
@@ -0,0 +1,12 @@
+package com.example.daedongyeojido_server.global.s3.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ImageUrlResponse {
+
+    private String imageUrl;
+}
+
