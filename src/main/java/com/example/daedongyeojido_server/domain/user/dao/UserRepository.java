39fafbfe44371448075e570df8fc7a3736db3d1a
김가은 <kge02160@gmail.com>
2024-02-08 19:06:09 +0900

    ♻️ :: findByName 추가

--- src/main/java/com/example/daedongyeojido_server/domain/user/dao/UserRepository.java
+++ src/main/java/com/example/daedongyeojido_server/domain/user/dao/UserRepository.java
@@ -10,5 +10,6 @@ import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByClassNumber(String classNumber);
+    Optional<User> findByName(String Name);
     List<User> findAllByMyClub(Club myClub);
 }
