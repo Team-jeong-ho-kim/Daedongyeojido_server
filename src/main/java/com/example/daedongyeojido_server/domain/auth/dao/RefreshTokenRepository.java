7a23762c61f82db586547cbfde9e59d154036a63
김가은 <kge02160@gmail.com>
2024-02-08 19:05:05 +0900

    ♻️ :: RefreshTokenRepository 어노테이션 추가 및 공백 줄 삭제

--- src/main/java/com/example/daedongyeojido_server/domain/auth/dao/RefreshTokenRepository.java
+++ src/main/java/com/example/daedongyeojido_server/domain/auth/dao/RefreshTokenRepository.java
@@ -2,10 +2,12 @@ package com.example.daedongyeojido_server.domain.auth.dao;
 
 import com.example.daedongyeojido_server.domain.auth.domain.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
     Optional<RefreshToken> findByToken(String token);
-}
 
+}
