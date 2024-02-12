2a5b99a30eac976cc15cf1437490995c0b3929cf
dkflfkd53 <b57062562@gmail.com>
2024-02-13 01:50:33 +0900

    🪓 :: 불필요한 auth api들 삭제

--- src/main/java/com/example/daedongyeojido_server/domain/auth/api/AuthController.java
+++ src/main/java/com/example/daedongyeojido_server/domain/auth/api/AuthController.java
@@ -1,13 +1,7 @@
 package com.example.daedongyeojido_server.domain.auth.api;
 
-import com.example.daedongyeojido_server.domain.auth.application.CheckUserService;
 import com.example.daedongyeojido_server.domain.auth.application.LoginService;
-import com.example.daedongyeojido_server.domain.auth.application.SignupService;
-import com.example.daedongyeojido_server.domain.auth.application.StudentLoginService;
-import com.example.daedongyeojido_server.domain.auth.application.TeacherLoginService;
-import com.example.daedongyeojido_server.domain.auth.dto.request.SignupAndCheckUserRequest;
-import com.example.daedongyeojido_server.domain.auth.dto.request.StudentLoginRequest;
-import com.example.daedongyeojido_server.domain.auth.dto.request.TeacherLoginRequest;
+import com.example.daedongyeojido_server.domain.auth.dto.request.LoginRequest;
 import com.example.daedongyeojido_server.domain.auth.dto.response.LoginResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -25,41 +19,9 @@ public class AuthController {
 
     private final LoginService loginService;
 
-    private final SignupService signupService;
-
-    private final StudentLoginService studentLoginService;
-
-    private final TeacherLoginService teacherLoginService;
-
-    private final CheckUserService checkUserService;
-
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/login")
-    public LoginResponse login(@RequestBody @Valid SignupAndCheckUserRequest request) {
+    public LoginResponse login(@RequestBody @Valid LoginRequest request) {
         return loginService.login(request);
-    }
-
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/signup")
-    public void signup(@RequestBody @Valid SignupAndCheckUserRequest request) {
-        signupService.signup(request);
-    }
-
-    @ResponseStatus(HttpStatus.OK)
-    @PostMapping("/student")
-    public LoginResponse studentLogin(@RequestBody @Valid StudentLoginRequest request) {
-        return studentLoginService.studentLogin(request);
-    }
-
-    @ResponseStatus(HttpStatus.OK)
-    @PostMapping("/teacher")
-    public LoginResponse teacherLogin(@RequestBody @Valid TeacherLoginRequest request) {
-        return teacherLoginService.teacherLogin(request);
-    }
-
-    @ResponseStatus(HttpStatus.OK)
-    @PostMapping("/check-user")
-    public boolean checkUser(@RequestBody @Valid SignupAndCheckUserRequest request) {
-        return checkUserService.checkUser(request);
     }
 }
