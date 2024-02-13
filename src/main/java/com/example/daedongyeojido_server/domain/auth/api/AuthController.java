a417edfa5491257e9168d7c2fa9759306695f979
김가은 <kge02160@gmail.com>
2024-02-13 22:40:46 +0900

    ♻️ :: reissue controller 추가

--- src/main/java/com/example/daedongyeojido_server/domain/auth/api/AuthController.java
+++ src/main/java/com/example/daedongyeojido_server/domain/auth/api/AuthController.java
@@ -1,16 +1,14 @@
 package com.example.daedongyeojido_server.domain.auth.api;
 
 import com.example.daedongyeojido_server.domain.auth.application.LoginService;
+import com.example.daedongyeojido_server.domain.auth.application.ReissueService;
 import com.example.daedongyeojido_server.domain.auth.dto.request.LoginRequest;
+import com.example.daedongyeojido_server.domain.auth.dto.request.RefreshTokenRequest;
 import com.example.daedongyeojido_server.domain.auth.dto.response.LoginResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,9 +17,16 @@ public class AuthController {
 
     private final LoginService loginService;
 
+    private final ReissueService reissueService;
+
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/login")
     public LoginResponse login(@RequestBody @Valid LoginRequest request) {
         return loginService.login(request);
+    }
+
+    @PatchMapping("/token")
+    public LoginResponse reissue(@RequestBody @Valid RefreshTokenRequest request) {
+        return reissueService.reissue(String.valueOf(request));
     }
 }
