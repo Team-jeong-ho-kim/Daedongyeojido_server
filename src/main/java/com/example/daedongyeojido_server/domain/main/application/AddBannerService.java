ddf5871bd4a37cdd70047f6377d225a5f12ca40c
dkflfkd53 <b57062562@gmail.com>
2024-02-13 14:52:56 +0900

    🌧 :: 배너 추가 api service 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/main/application/AddBannerService.java
@@ -0,0 +1,23 @@
+package com.example.daedongyeojido_server.domain.main.application;
+
+import com.example.daedongyeojido_server.domain.main.application.dao.BannerRepository;
+import com.example.daedongyeojido_server.domain.main.domain.Banner;
+import com.example.daedongyeojido_server.domain.main.dto.request.BannerRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AddBannerService {
+
+    private final BannerRepository bannerRepository;
+
+    @Transactional
+    public void addBanner(BannerRequest request) {
+        bannerRepository.save(
+                Banner.builder()
+                        .bannerImgUrl(request.getBannerImgUrl())
+                        .build());
+    }
+}
