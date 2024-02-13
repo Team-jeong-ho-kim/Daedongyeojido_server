3f7b66e5e1630822d27bf95bb03d0d5d5f675de4
dkflfkd53 <b57062562@gmail.com>
2024-02-13 14:52:23 +0900

    🌧 :: 배너 repository 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/main/application/dao/BannerRepository.java
@@ -0,0 +1,7 @@
+package com.example.daedongyeojido_server.domain.main.application.dao;
+
+import com.example.daedongyeojido_server.domain.main.domain.Banner;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BannerRepository extends JpaRepository<Banner, Long> {
+}
