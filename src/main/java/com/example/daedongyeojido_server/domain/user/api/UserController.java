793d7e34921ea258fccf90ae82c8fe131f1a6414
dkflfkd53 <b57062562@gmail.com>
2024-02-15 01:45:59 +0900

    🌧 :: 유저 전체 조회 api controller 추가

--- src/main/java/com/example/daedongyeojido_server/domain/user/api/UserController.java
+++ src/main/java/com/example/daedongyeojido_server/domain/user/api/UserController.java
@@ -1,9 +1,11 @@
 package com.example.daedongyeojido_server.domain.user.api;
 
 import com.example.daedongyeojido_server.domain.user.application.AddClubMemberService;
+import com.example.daedongyeojido_server.domain.user.application.FindAllUserService;
 import com.example.daedongyeojido_server.domain.user.application.StudentInfoService;
 import com.example.daedongyeojido_server.domain.user.application.TeacherInfoService;
 import com.example.daedongyeojido_server.domain.user.dto.request.AddClubMemberRequest;
+import com.example.daedongyeojido_server.domain.user.dto.response.AllUserResponse;
 import com.example.daedongyeojido_server.domain.user.dto.response.MyInfoResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,6 +28,8 @@ public class UserController {
     private final TeacherInfoService teacherInfoService;
 
     private final AddClubMemberService addClubMemberService;
+
+    private final FindAllUserService findAllUserService;
 
     @ResponseStatus(HttpStatus.OK)
     @PostMapping("/student-info")
@@ -41,5 +47,11 @@ public class UserController {
     @PostMapping("/member")
     public void addClubMember(@RequestBody @Valid AddClubMemberRequest request) {
         addClubMemberService.AddClubMember(request);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/all")
+    public List<AllUserResponse> findAllUser() {
+        return findAllUserService.findAllUser();
     }
 }
