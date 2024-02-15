7b3e42b60b8d4aa80009ab42b0753b72cab13b03
dkflfkd53 <b57062562@gmail.com>
2024-02-15 14:28:16 +0900

    ♻️ :: 마이페이지 api service 수정

--- src/main/java/com/example/daedongyeojido_server/domain/user/application/MyInfoService.java
+++ src/main/java/com/example/daedongyeojido_server/domain/user/application/MyInfoService.java
@@ -1,20 +1,27 @@
 package com.example.daedongyeojido_server.domain.user.application;
 
+import com.example.daedongyeojido_server.domain.report.dao.ReportRepository;
 import com.example.daedongyeojido_server.domain.user.domain.User;
 import com.example.daedongyeojido_server.domain.user.dto.response.MyInfoResponse;
+import com.example.daedongyeojido_server.domain.user.dto.response.MyReportResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
-public class TeacherInfoService {
+public class MyInfoService {
 
     private final UserFacade userFacade;
 
+    private final ReportRepository reportRepository;
+
     @Transactional(readOnly = true)
-    public MyInfoResponse teacherInfo() {
-        User user = userFacade.currentTeacher();
+    public MyInfoResponse myInfo() {
+        User user = userFacade.currentUser();
 
         String clubName;
         if (user.getMyClub() == null) {
@@ -23,11 +30,17 @@ public class TeacherInfoService {
             clubName = user.getMyClub().getClubName();
         }
 
+        List<MyReportResponse> myReportResponses = reportRepository.findAllByClassNumber(user.getClassNumber())
+                .stream()
+                .map(MyReportResponse::new)
+                .collect(Collectors.toList());
+
         return MyInfoResponse.builder()
                 .classNumber(user.getClassNumber())
                 .name(user.getName())
                 .part(user.getPart())
                 .myClub(clubName)
+                .myReport(myReportResponses)
                 .build();
     }
 }
