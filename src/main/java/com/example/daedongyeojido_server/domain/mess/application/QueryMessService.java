7e075eb0593986303369a31c1ab96ceff9196660
dkflfkd53 <b57062562@gmail.com>
2024-02-15 16:18:02 +0900

    🌧 :: 수락/미수락 확인 로직 추가

--- src/main/java/com/example/daedongyeojido_server/domain/mess/application/QueryMessService.java
+++ src/main/java/com/example/daedongyeojido_server/domain/mess/application/QueryMessService.java
@@ -1,7 +1,10 @@
 package com.example.daedongyeojido_server.domain.mess.application;
 
 import com.example.daedongyeojido_server.domain.mess.dao.MessRepository;
+import com.example.daedongyeojido_server.domain.mess.domain.Mess;
 import com.example.daedongyeojido_server.domain.mess.dto.response.QueryMessResponse;
+import com.example.daedongyeojido_server.domain.user.application.UserFacade;
+import com.example.daedongyeojido_server.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,14 +16,28 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class QueryMessService {
 
+    private final UserFacade userFacade;
+
     private final MessRepository messRepository;
 
     @Transactional(readOnly = true)
     public List<QueryMessResponse> queryMess() {
 
-        return messRepository.findAll()
+        User teacher = userFacade.currentUser();
+
+        List<Mess> messes = messRepository.findAll();
+
+        List<QueryMessResponse> queryMessResponses = messRepository.findAll()
                 .stream()
                 .map(QueryMessResponse::new)
                 .collect(Collectors.toList());
+
+        for(int i=0; i<queryMessResponses.size(); i++) {
+            if(messes.get(i).getAcceptTeachers().contains(teacher.getName())) {
+                queryMessResponses.get(i).accept();
+            }
+        }
+
+        return queryMessResponses;
     }
 }
