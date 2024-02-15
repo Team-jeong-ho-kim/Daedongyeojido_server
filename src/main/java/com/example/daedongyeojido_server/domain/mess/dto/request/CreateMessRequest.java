d742b4f130faf5d6e350764f743448cd58f295d6
dkflfkd53 <b57062562@gmail.com>
2024-02-15 16:18:32 +0900

    ♻️ :: 회식 새성 request 제약조건 수정

--- src/main/java/com/example/daedongyeojido_server/domain/mess/dto/request/CreateMessRequest.java
+++ src/main/java/com/example/daedongyeojido_server/domain/mess/dto/request/CreateMessRequest.java
@@ -1,7 +1,6 @@
 package com.example.daedongyeojido_server.domain.mess.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,14 +11,12 @@ import java.time.LocalDate;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CreateMessRequest {
 
-    @NotBlank
-    @Size(min = 1, max = 2, message = "시작 교시는 한 자리수 또는 두 자리수 중에서 선택해야하며 선택을 하지 않으면 안됩니다.")
+    @NotNull
     private Integer messStartTime;
 
-    @NotBlank
-    @Size(min = 1, max = 2, message = "마무리 교시는 한 자리수 또는 두 자리수 중에서 선택해야하며 선택을 하지 않으면 안됩니다.")
+    @NotNull
     private Integer messEndTime;
 
-    @NotBlank
+    @NotNull
     private LocalDate messDate;
 }
