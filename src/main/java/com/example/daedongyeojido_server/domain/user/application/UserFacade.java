67e49993cf6b8abf4b55e97bc1b92fe822ce54a9
dkflfkd53 <b57062562@gmail.com>
2024-02-15 14:11:09 +0900

    ♻️ :: UserFacade 수정

--- src/main/java/com/example/daedongyeojido_server/domain/user/application/UserFacade.java
+++ src/main/java/com/example/daedongyeojido_server/domain/user/application/UserFacade.java
@@ -1,6 +1,5 @@
 package com.example.daedongyeojido_server.domain.user.application;
 
-import com.example.daedongyeojido_server.domain.user.dao.CustomUserRepository;
 import com.example.daedongyeojido_server.domain.user.dao.UserRepository;
 import com.example.daedongyeojido_server.domain.user.domain.User;
 import com.example.daedongyeojido_server.domain.user.exception.NotAuthenticatedException;
@@ -8,7 +7,6 @@ import com.example.daedongyeojido_server.domain.user.exception.UserNotFoundExcep
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,37 +15,16 @@ public class UserFacade {
 
     private final UserRepository userRepository;
 
-    private final CustomUserRepository customUserRepository;
-
-    public User currentStudent() {
+    public User currentUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (authentication == null || !authentication.isAuthenticated()) {
-            throw new NotAuthenticatedException("인증되지 않는 학생입니다.");
+            throw new NotAuthenticatedException("인증되지 않는 유저입니다.");
         }
 
-        String classNumber = authentication.getName();
+        String xquareId = authentication.getName();
 
-        return userRepository.findByClassNumber(classNumber)
-                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
-    }
-
-    public User currentTeacher() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        if (authentication == null || !authentication.isAuthenticated()) {
-            throw new NotAuthenticatedException("인증되지 않는 선생님입니다.");
-        }
-
-        Object principal = authentication.getPrincipal();
-        if (!(principal instanceof UserDetails)) {
-            throw new NotAuthenticatedException("인증되지 않는 선생님입니다.");
-        }
-
-        UserDetails userDetails = (UserDetails) principal;
-        String name = userDetails.getPassword();
-
-        return customUserRepository.findTeacherByName(name)
+        return userRepository.findByXquareId(xquareId)
                 .orElseThrow(() -> UserNotFoundException.EXCEPTION);
     }
 }
