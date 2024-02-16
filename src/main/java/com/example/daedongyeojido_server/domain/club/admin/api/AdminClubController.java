c0734ea5e716b7e0b6858e98b5e210a65f2b5328
dkflfkd53 <b57062562@gmail.com>
2024-02-16 13:55:28 +0900

    🌧 :: 동아리 인원 수정 api controller 추가

--- src/main/java/com/example/daedongyeojido_server/domain/club/admin/api/AdminClubController.java
+++ src/main/java/com/example/daedongyeojido_server/domain/club/admin/api/AdminClubController.java
@@ -2,13 +2,16 @@ package com.example.daedongyeojido_server.domain.club.admin.api;
 
 import com.example.daedongyeojido_server.domain.club.admin.application.CreateClubService;
 import com.example.daedongyeojido_server.domain.club.admin.application.DeleteClubService;
+import com.example.daedongyeojido_server.domain.club.admin.application.EditClubMemberService;
 import com.example.daedongyeojido_server.domain.club.admin.application.QueryAdminPageService;
 import com.example.daedongyeojido_server.domain.club.admin.dto.request.CreateClubRequest;
+import com.example.daedongyeojido_server.domain.club.admin.dto.request.EditClubMemberRequest;
 import com.example.daedongyeojido_server.domain.club.admin.dto.response.AdminPageResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,6 +30,8 @@ public class AdminClubController {
 
     private final QueryAdminPageService queryAdminPageService;
 
+    private final EditClubMemberService editClubMemberService;
+
     private final DeleteClubService deleteClubService;
 
     @ResponseStatus(HttpStatus.CREATED)
@@ -39,6 +44,12 @@ public class AdminClubController {
     @PostMapping("/page")
     public List<AdminPageResponse> queryClubList() {
         return queryAdminPageService.queryAdminPage();
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PatchMapping("/edit")
+    public void editClub(@RequestBody @Valid EditClubMemberRequest request) {
+        editClubMemberService.editClubMember(request);
     }
 
     @ResponseStatus(HttpStatus.OK)
