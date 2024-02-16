0922f1d77f1c5520b70051ea096b029bc5f50bca
dkflfkd53 <b57062562@gmail.com>
2024-02-16 13:01:05 +0900

    🪓 :: 동아리원 추가 api 삭제

--- src/main/java/com/example/daedongyeojido_server/domain/user/api/UserController.java
+++ src/main/java/com/example/daedongyeojido_server/domain/user/api/UserController.java
@@ -1,17 +1,13 @@
 package com.example.daedongyeojido_server.domain.user.api;
 
-import com.example.daedongyeojido_server.domain.user.application.AddClubMemberService;
 import com.example.daedongyeojido_server.domain.user.application.FindAllUserService;
 import com.example.daedongyeojido_server.domain.user.application.MyInfoService;
-import com.example.daedongyeojido_server.domain.user.dto.request.AddClubMemberRequest;
 import com.example.daedongyeojido_server.domain.user.dto.response.AllUserResponse;
 import com.example.daedongyeojido_server.domain.user.dto.response.MyInfoResponse;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,20 +21,12 @@ public class UserController {
 
     private final MyInfoService myInfoService;
 
-    private final AddClubMemberService addClubMemberService;
-
     private final FindAllUserService findAllUserService;
 
     @ResponseStatus(HttpStatus.OK)
     @PostMapping("/my-info")
     public MyInfoResponse myInfo() {
         return myInfoService.myInfo();
-    }
-
-    @ResponseStatus(HttpStatus.OK)
-    @PostMapping("/member")
-    public void addClubMember(@RequestBody @Valid AddClubMemberRequest request) {
-        addClubMemberService.AddClubMember(request);
     }
 
     @ResponseStatus(HttpStatus.OK)
