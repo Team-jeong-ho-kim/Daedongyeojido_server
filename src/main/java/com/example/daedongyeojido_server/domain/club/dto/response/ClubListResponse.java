2f16c2170c69d6037c29d795f8accdcd624bc409
dkflfkd53 <b57062562@gmail.com>
2024-02-16 12:14:20 +0900

    🌧 :: 관리자 페이지 조회 api response 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/club/dto/response/ClubListResponse.java
@@ -0,0 +1,34 @@
+package com.example.daedongyeojido_server.domain.club.dto.response;
+
+import com.example.daedongyeojido_server.domain.club.domain.Club;
+import com.example.daedongyeojido_server.domain.user.dto.response.MemberResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@AllArgsConstructor
+public class ClubListResponse {
+
+    private String clubName;
+
+    private String teacherName;
+
+    private int messCount = 0;
+
+    private List<MemberResponse> memberResponses;
+
+    public ClubListResponse(Club club) {
+        if(club.getTeacher() == null) teacherName = "null";
+        else teacherName = club.getTeacher().getName();
+
+        clubName = club.getClubName();
+        messCount = club.getMessCount();
+        memberResponses = club.getClubMembers()
+                .stream()
+                .map(MemberResponse::new)
+                .collect(Collectors.toList());
+    }
+}
