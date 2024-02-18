91989506915540972eae9c3a1fe81f474fe80070
dkflfkd53 <b57062562@gmail.com>
2024-02-18 17:34:20 +0900

    ♻️ :: 로그인 api 수정

--- src/main/java/com/example/daedongyeojido_server/domain/auth/application/LoginService.java
+++ src/main/java/com/example/daedongyeojido_server/domain/auth/application/LoginService.java
@@ -6,6 +6,8 @@ import com.example.daedongyeojido_server.domain.user.dao.UserRepository;
 import com.example.daedongyeojido_server.domain.user.domain.User;
 import com.example.daedongyeojido_server.domain.user.domain.enums.Part;
 import com.example.daedongyeojido_server.global.security.jwt.JwtTokenProvider;
+import com.example.daedongyeojido_server.infra.feign.XquareClient;
+import com.example.daedongyeojido_server.infra.feign.dto.response.XquareUserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,16 +20,29 @@ public class LoginService {
 
     private final JwtTokenProvider jwtTokenProvider;
 
+    private final XquareClient xquareClient;
+
     @Transactional
     public LoginResponse login(LoginRequest request) {
+
+        XquareUserResponse xquareUser = xquareClient.xquareUser(request.getXquareId());
+
+        String num = "null";
+        if(xquareUser.getNum()<10) num = "0" + String.valueOf(xquareUser.getNum());
+
+        String classNumber = String.valueOf(xquareUser.getGrade()) + String.valueOf(xquareUser.getClass_num()) + num;
+
+        Part part = Part.ERROR;
+        if(xquareUser.getUser_role().equals("STU")) part = Part.INDEPENDENT;
+        else if (xquareUser.getUser_role().equals("SCH")) part = Part.TEACHER;
 
         if(userRepository.findByXquareId(request.getXquareId()).isPresent()) {
             return jwtTokenProvider.receiveToken(request.getXquareId());
         }
-        else if (request.getName().equals("신요셉")) {
+        else if (xquareUser.getName().equals("신요셉")) {
             User user = User.builder()
                     .xquareId(request.getXquareId())
-                    .name(request.getName())
+                    .name(xquareUser.getName())
                     .part(Part.CLUB_LEADER_TEACHER)
                     .build();
 
@@ -36,9 +51,9 @@ public class LoginService {
         else {
             User user = User.builder()
                     .xquareId(request.getXquareId())
-                    .classNumber(request.getClassNumber())
-                    .name(request.getName())
-                    .part(request.getPart())
+                    .classNumber(classNumber)
+                    .name(xquareUser.getName())
+                    .part(part)
                     .build();
 
             userRepository.save(user);
--- src/main/java/com/example/daedongyeojido_server/domain/auth/dto/request/LoginRequest.java
+++ src/main/java/com/example/daedongyeojido_server/domain/auth/dto/request/LoginRequest.java
@@ -1,9 +1,6 @@
 package com.example.daedongyeojido_server.domain.auth.dto.request;
 
-import com.example.daedongyeojido_server.domain.user.domain.enums.Part;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,14 +11,4 @@ public class LoginRequest {
 
     @NotBlank
     private String xquareId;
-
-    @Size(min = 4, max = 4, message = "학번은 4글자 이어야 합니다.")
-    private String classNumber;
-
-    @NotBlank
-    @Size(min = 3, max = 4, message = "이름은 3글자 이상, 4글자 이하여야 합니다.")
-    private String name;
-
-    @NotNull
-    private Part part;
 }
