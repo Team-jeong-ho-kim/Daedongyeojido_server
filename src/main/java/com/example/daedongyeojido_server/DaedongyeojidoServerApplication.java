1d42cca2e46e4b556cbff640a7ed174b8591dbb1
dkflfkd53 <b57062562@gmail.com>
2024-02-18 17:34:03 +0900

    🌧 :: OpenFeign 스캔 어노테이션 추가

--- src/main/java/com/example/daedongyeojido_server/DaedongyeojidoServerApplication.java
+++ src/main/java/com/example/daedongyeojido_server/DaedongyeojidoServerApplication.java
@@ -1,12 +1,17 @@
 package com.example.daedongyeojido_server;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableFeignClients
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class DaedongyeojidoServerApplication {
 
 	public static void main(String[] args) {
