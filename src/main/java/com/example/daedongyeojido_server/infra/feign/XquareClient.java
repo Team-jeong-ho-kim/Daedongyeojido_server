d61eeddddc3c1ef189163bee10645d259cb69b87
dkflfkd53 <b57062562@gmail.com>
2024-02-18 17:45:07 +0900

    ♻️ :: XquareUrl 환경변수로 변경

--- src/main/java/com/example/daedongyeojido_server/infra/feign/XquareClient.java
+++ src/main/java/com/example/daedongyeojido_server/infra/feign/XquareClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "xquare-client", url = "https://prod-server.xquare.app/users/account-id")
+@FeignClient(name = "xquare-client", url = "${key.xquare-api-url}")
 public interface XquareClient {
 
     @GetMapping("/{accountId}")
