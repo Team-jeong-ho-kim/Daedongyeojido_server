919cdc6f2b861a9551931efc3069de2b927b2f6f
dkflfkd53 <b57062562@gmail.com>
2024-02-19 18:34:51 +0900

    ♻️ :: 테스트를 위한 권한 해제

--- src/main/java/com/example/daedongyeojido_server/global/config/SecurityConfig.java
+++ src/main/java/com/example/daedongyeojido_server/global/config/SecurityConfig.java
@@ -58,11 +58,11 @@ public class SecurityConfig {
                         .requestMatchers("/club/modify", "/notice/create", "/notice/delete/**", "/mess/create", "/mess/delete/**", "/alarm/result")
                         .hasRole("CLUB_LEADER")
 
-                        //.requestMatchers("/admin-club/page", "/admin-club/edit-member", "/mess/accept/**", "/mess/all")
-                       // .hasAnyRole("TEACHER", "CLUB_LEADER_TEACHER")
+                        .requestMatchers("/admin-club/page", "/admin-club/edit-member", "/mess/accept/**", "/mess/all")
+                        .hasAnyRole("TEACHER", "CLUB_LEADER_TEACHER")
 
-                        .requestMatchers("/admin-club/create", "/admin-club/delete/**", "/alarm/announcement"
-                        ).hasRole("CLUB_LEADER_TEACHER")
+//                        .requestMatchers("/admin-club/create", "/admin-club/delete/**", "/alarm/announcement"
+//                        ).hasRole("CLUB_LEADER_TEACHER")
 
                         .anyRequest().authenticated()
                 )
