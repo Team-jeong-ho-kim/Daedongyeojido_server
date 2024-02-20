84276f79ed38884df4797742e7a30dd44cc7c9e0
dkflfkd53 <b57062562@gmail.com>
2024-02-20 20:45:00 +0900

    ♻️ :: banner api secretKey로 권한 확인 로직 추가

--- src/main/java/com/example/daedongyeojido_server/domain/main/application/AddBannerService.java
+++ src/main/java/com/example/daedongyeojido_server/domain/main/application/AddBannerService.java
@@ -3,7 +3,9 @@ package com.example.daedongyeojido_server.domain.main.application;
 import com.example.daedongyeojido_server.domain.main.dao.BannerRepository;
 import com.example.daedongyeojido_server.domain.main.domain.Banner;
 import com.example.daedongyeojido_server.domain.main.dto.request.BannerRequest;
+import com.example.daedongyeojido_server.domain.user.exception.InvalidKeyException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,8 +15,14 @@ public class AddBannerService {
 
     private final BannerRepository bannerRepository;
 
+    @Value("${key.daedong-secret-key}")
+    private String secretKey;
+
     @Transactional
     public void addBanner(BannerRequest request) {
+
+        if (!secretKey.equals(request.getSecretKey())) throw InvalidKeyException.EXCEPTION;
+
         bannerRepository.save(
                 Banner.builder()
                         .bannerImgUrl(request.getBannerImgUrl())
--- src/main/java/com/example/daedongyeojido_server/domain/main/application/DeleteBannerService.java
+++ src/main/java/com/example/daedongyeojido_server/domain/main/application/DeleteBannerService.java
@@ -1,7 +1,10 @@
 package com.example.daedongyeojido_server.domain.main.application;
 
 import com.example.daedongyeojido_server.domain.main.dao.BannerRepository;
+import com.example.daedongyeojido_server.domain.main.dto.request.DeleteBannerRequest;
+import com.example.daedongyeojido_server.domain.user.exception.InvalidKeyException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,8 +14,14 @@ public class DeleteBannerService {
 
     private final BannerRepository bannerRepository;
 
+    @Value("${key.daedong-secret-key}")
+    private String secretKey;
+
     @Transactional
-    public void deleteBanner(Long bannerId) {
-        bannerRepository.deleteById(bannerId);
+    public void deleteBanner(DeleteBannerRequest request) {
+
+        if (!secretKey.equals(request.getSecretKey())) throw InvalidKeyException.EXCEPTION;
+
+        bannerRepository.deleteById(request.getBannerId());
     }
 }
