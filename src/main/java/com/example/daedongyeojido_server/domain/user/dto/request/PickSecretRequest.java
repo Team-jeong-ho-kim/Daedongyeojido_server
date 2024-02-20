1cb765ff09fbb461bbbaa2cf71468045cf610d3a
dkflfkd53 <b57062562@gmail.com>
2024-02-20 20:33:44 +0900

    🌧 :: pick-secre-key request 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/user/dto/request/PickSecretRequest.java
@@ -0,0 +1,14 @@
+package com.example.daedongyeojido_server.domain.user.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PickSecretRequest {
+
+    @NotBlank
+    private String pickSecretKey;
+}
