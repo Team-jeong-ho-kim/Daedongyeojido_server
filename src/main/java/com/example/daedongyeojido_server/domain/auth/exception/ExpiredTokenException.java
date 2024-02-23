1e4febf1fc3815840503ab29b8267b11b04fff9f
Kgeaun <kge02160@gmail.com>
2024-02-23 15:24:40 +0900

    ♻️ :: 공백줄 제거 및 간격 맞춤

--- src/main/java/com/example/daedongyeojido_server/domain/auth/exception/ExpiredTokenException.java
+++ src/main/java/com/example/daedongyeojido_server/domain/auth/exception/ExpiredTokenException.java
@@ -7,7 +7,7 @@ public class ExpiredTokenException extends BusinessException {
 
     public static final BusinessException EXCEPTION = new ExpiredTokenException();
 
-    public ExpiredTokenException(){
+    public ExpiredTokenException() {
         super(ErrorCode.EXPIRED_TOKEN);
     }
 }
