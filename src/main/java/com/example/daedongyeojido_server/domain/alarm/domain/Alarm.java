964df3a36ef8d4b2c3a3109d6be76867593de4f7
Kgeaun <kge02160@gmail.com>
2024-02-23 14:19:42 +0900

    ♻️ :: 사용 안 하는 import 제거

--- src/main/java/com/example/daedongyeojido_server/domain/alarm/domain/Alarm.java
+++ src/main/java/com/example/daedongyeojido_server/domain/alarm/domain/Alarm.java
@@ -4,21 +4,8 @@ import com.example.daedongyeojido_server.domain.alarm.domain.enums.AlarmType;
 import com.example.daedongyeojido_server.domain.notice.domain.enums.Major;
 import com.example.daedongyeojido_server.domain.report.domain.enums.PassingResult;
 import com.example.daedongyeojido_server.domain.user.domain.User;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Getter
