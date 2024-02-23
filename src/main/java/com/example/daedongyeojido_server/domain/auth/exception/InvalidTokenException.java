9d79443689f983a5896a98ea26a33dd40b56f936
Kgeaun <kge02160@gmail.com>
2024-02-23 15:24:54 +0900

    ♻️ :: 공백줄 제거 및 간격 맞춤

--- src/main/java/com/example/daedongyeojido_server/domain/auth/exception/InvalidTokenException.java
+++ src/main/java/com/example/daedongyeojido_server/domain/auth/exception/InvalidTokenException.java
@@ -7,7 +7,7 @@ public class InvalidTokenException extends BusinessException {
 
     public static final BusinessException EXCEPTION = new InvalidTokenException();
 
-    public InvalidTokenException(){
+    public InvalidTokenException() {
         super(ErrorCode.INVALID_TOKEN);
     }
 }
