2da8eae8711c7a8a6ecbf7c70075143fb56e35f7
Kgeaun <kge02160@gmail.com>
2024-02-23 15:21:58 +0900

    ♻️ :: 공백줄 제거

--- src/main/java/com/example/daedongyeojido_server/domain/auth/dao/RefreshTokenRepository.java
+++ src/main/java/com/example/daedongyeojido_server/domain/auth/dao/RefreshTokenRepository.java
@@ -9,5 +9,4 @@ import java.util.Optional;
 @Repository
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
     Optional<RefreshToken> findByToken(String token);
-
 }
