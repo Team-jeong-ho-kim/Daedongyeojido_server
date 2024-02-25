e644dd4b1de80cb3e3b1c8cf6420647314ce8b4d
dkflfkd53 <b57062562@gmail.com>
2024-02-25 22:04:15 +0900

    🌧 :: 공지사항 조회 api service 추가

--- src/main/java/com/example/daedongyeojido_server/domain/alarm/application/QueryAnnouncementService.java
+++ src/main/java/com/example/daedongyeojido_server/domain/alarm/application/QueryAnnouncementService.java
@@ -1,2 +1,24 @@
-package com.example.daedongyeojido_server.domain.alarm.application;public class QueryAnnouncementService {
+package com.example.daedongyeojido_server.domain.alarm.application;
+
+import com.example.daedongyeojido_server.domain.alarm.dao.AlarmRepository;
+import com.example.daedongyeojido_server.domain.alarm.domain.enums.AlarmType;
+import com.example.daedongyeojido_server.domain.alarm.dto.response.AnnouncementResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class QueryAnnouncementService {
+
+    private final AlarmRepository alarmRepository;
+
+    public List<AnnouncementResponse> queryAnnouncement() {
+        return alarmRepository.findAllByAlarmType(AlarmType.ANNOUNCEMENT)
+                .stream()
+                .map(AnnouncementResponse::new)
+                .collect(Collectors.toList());
+    }
 }
