0da80fefeb94918eea05ff959c2be45f3843ae82
dkflfkd53 <b57062562@gmail.com>
2024-02-27 03:13:56 +0900

    🌧 :: 면접 시간 정하기 api service 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/report/application/ChooseInterviewTimeService.java
@@ -0,0 +1,22 @@
+package com.example.daedongyeojido_server.domain.report.application;
+
+import com.example.daedongyeojido_server.domain.report.application.facade.ReportFacade;
+import com.example.daedongyeojido_server.domain.report.domain.Report;
+import com.example.daedongyeojido_server.domain.report.dto.request.InterviewTimeRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ChooseInterviewTimeService {
+
+    private final ReportFacade reportFacade;
+
+    @Transactional
+    public void chooseInterviewTime(InterviewTimeRequest request) {
+        Report report = reportFacade.reportFacade(request.getReportId());
+
+        report.saveInterviewTime(request.getInterviewStartTime(), request.getInterviewEndTime());
+    }
+}
