093df8c527c17c1451347cac8f19f10f90e0723d
dkflfkd53 <b57062562@gmail.com>
2024-02-27 01:56:41 +0900

    🌧 :: 메모 조회 api 컨트롤러 추가

--- src/main/java/com/example/daedongyeojido_server/domain/report/api/ReportController.java
+++ src/main/java/com/example/daedongyeojido_server/domain/report/api/ReportController.java
@@ -3,8 +3,10 @@ package com.example.daedongyeojido_server.domain.report.api;
 import com.example.daedongyeojido_server.domain.report.application.ApplyService;
 import com.example.daedongyeojido_server.domain.report.application.CancelApplyService;
 import com.example.daedongyeojido_server.domain.report.application.QueryApplicantService;
+import com.example.daedongyeojido_server.domain.report.application.QueryMemoService;
 import com.example.daedongyeojido_server.domain.report.application.ReportInfoService;
 import com.example.daedongyeojido_server.domain.report.dto.request.ApplyRequest;
+import com.example.daedongyeojido_server.domain.report.dto.response.MemoResponse;
 import com.example.daedongyeojido_server.domain.report.dto.response.ReportInfoResponse;
 import com.example.daedongyeojido_server.domain.report.dto.response.ReportResponse;
 import jakarta.validation.Valid;
@@ -32,6 +34,8 @@ public class ReportController {
 
     private final ReportInfoService reportInfoService;
 
+    private final QueryMemoService queryMemoService;
+
     private final CancelApplyService cancelApplyService;
 
     @ResponseStatus(HttpStatus.CREATED)
@@ -50,6 +54,12 @@ public class ReportController {
     @GetMapping("/report-info/{reportId}")
     public ReportInfoResponse reportInfo(@PathVariable Long reportId) {
         return reportInfoService.reportInfo(reportId);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/memo/{reportId}")
+    public MemoResponse queryMemo(@PathVariable Long reportId) {
+        return queryMemoService.queryMemo(reportId);
     }
 
     @ResponseStatus(HttpStatus.OK)
