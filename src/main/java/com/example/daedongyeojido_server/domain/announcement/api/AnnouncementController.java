d5e5f57fcfbc96e0ca348ecae1c2d9918240041b
dkflfkd53 <b57062562@gmail.com>
2024-02-26 17:11:45 +0900

    🌧 :: 공지사항 컨트롤러 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/announcement/api/AnnouncementController.java
@@ -0,0 +1,39 @@
+package com.example.daedongyeojido_server.domain.announcement.api;
+
+import com.example.daedongyeojido_server.domain.announcement.application.CreateAnnouncementService;
+import com.example.daedongyeojido_server.domain.announcement.application.QueryAnnouncementService;
+import com.example.daedongyeojido_server.domain.announcement.dto.request.CreateAnnouncementRequest;
+import com.example.daedongyeojido_server.domain.announcement.dto.response.AnnouncementResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/announcement")
+public class AnnouncementController {
+
+    private final CreateAnnouncementService createAnnouncementService;
+
+    private final QueryAnnouncementService queryAnnouncementService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/create")
+    public void createAnnouncement(@RequestBody @Valid CreateAnnouncementRequest request) {
+        createAnnouncementService.createAnnouncement(request);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/query")
+    public List<AnnouncementResponse> queryAnnouncement() {
+        return queryAnnouncementService.queryAnnouncement();
+    }
+}
