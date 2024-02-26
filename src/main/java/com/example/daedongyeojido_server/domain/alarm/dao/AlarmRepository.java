d8974f0539c9fa2a8786a44c89024ee67957b92b
dkflfkd53 <b57062562@gmail.com>
2024-02-26 17:10:40 +0900

    ♻️ :: 알람 레포지토리 메서드 삭제

--- src/main/java/com/example/daedongyeojido_server/domain/alarm/dao/AlarmRepository.java
+++ src/main/java/com/example/daedongyeojido_server/domain/alarm/dao/AlarmRepository.java
@@ -1,7 +1,6 @@
 package com.example.daedongyeojido_server.domain.alarm.dao;
 
 import com.example.daedongyeojido_server.domain.alarm.domain.Alarm;
-import com.example.daedongyeojido_server.domain.alarm.domain.enums.AlarmType;
 import com.example.daedongyeojido_server.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,6 +8,4 @@ import java.util.List;
 
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
     List<Alarm> findAllByUser(User user);
-
-    List<Alarm> findAllByAlarmType(AlarmType alarmType);
 }
