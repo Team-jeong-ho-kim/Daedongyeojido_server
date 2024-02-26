4a3c1c4fc22875daf6414b6dd40f6f446bbd32d9
dkflfkd53 <b57062562@gmail.com>
2024-02-26 21:56:37 +0900

    ♻️ :: 동아리 수정 api 수정

--- src/main/java/com/example/daedongyeojido_server/domain/club/common/application/ModifyClubService.java
+++ src/main/java/com/example/daedongyeojido_server/domain/club/common/application/ModifyClubService.java
@@ -3,6 +3,8 @@ package com.example.daedongyeojido_server.domain.club.common.application;
 import com.example.daedongyeojido_server.domain.club.common.dao.ClubRepository;
 import com.example.daedongyeojido_server.domain.club.common.domain.Club;
 import com.example.daedongyeojido_server.domain.club.common.dto.request.ModifyClubRequest;
+import com.example.daedongyeojido_server.domain.club.common.exception.ClubMisMatchException;
+import com.example.daedongyeojido_server.domain.user.application.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,10 +13,14 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ModifyClubService {
 
+    private final UserFacade userFacade;
+
     private final ClubRepository clubRepository;
 
     @Transactional
     public void modifyClub(ModifyClubRequest request) {
+        if(!(userFacade.currentUser().getMyClub().getClubName().equals(request.getClubName()))) throw ClubMisMatchException.EXCEPTION;
+
         Club club = clubRepository.findByClubName(request.getClubName());
 
         club.modifyClub(request.getTitle(), request.getIntroduction(), request.getProject(),
