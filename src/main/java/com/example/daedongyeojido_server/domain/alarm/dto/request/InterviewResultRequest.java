b1b96d9ada50f4cb581ab235b00a5ab6604c4069
dkflfkd53 <b57062562@gmail.com>
2024-02-27 02:54:23 +0900

    ♻️ :: 면접 결과 주기 api 이름 변경

--- src/main/java/com/example/daedongyeojido_server/domain/alarm/dto/request/InterviewResultRequest.java
+++ src/main/java/com/example/daedongyeojido_server/domain/alarm/dto/request/InterviewResultRequest.java
@@ -1,5 +1,6 @@
 package com.example.daedongyeojido_server.domain.alarm.dto.request;
 
+import com.example.daedongyeojido_server.domain.alarm.domain.enums.AlarmType;
 import com.example.daedongyeojido_server.domain.report.domain.enums.PassingResult;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
@@ -8,11 +9,14 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ReportResultRequest {
+public class InterviewResultRequest {
 
     @NotNull
     private Long reportId;
 
     @NotNull
     private PassingResult passingResult;
+
+    @NotNull
+    private AlarmType alarmType;
 }
