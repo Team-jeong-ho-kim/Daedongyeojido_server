fd69b2c67ad3dfc67cf47119453a144e51d96eee
dkflfkd53 <b57062562@gmail.com>
2024-02-28 11:11:23 +0900

    🌧 :: findAllByClubName 메서드 추가

--- src/main/java/com/example/daedongyeojido_server/domain/club/common/dao/InterviewTimeRepository.java
+++ src/main/java/com/example/daedongyeojido_server/domain/club/common/dao/InterviewTimeRepository.java
@@ -3,5 +3,8 @@ package com.example.daedongyeojido_server.domain.club.common.dao;
 import com.example.daedongyeojido_server.domain.club.common.domain.InterviewTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface InterviewTimeRepository extends JpaRepository<InterviewTime, Long> {
+    List<InterviewTime> findAllByClubName(String clubName);
 }
