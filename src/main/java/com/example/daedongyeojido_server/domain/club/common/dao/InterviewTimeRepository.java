9ac2a9d0b3ffcbde27ce032d624c4c28454ac306
dkflfkd53 <b57062562@gmail.com>
2024-02-28 11:01:15 +0900

    🌧 :: 면접 시간 repository 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/club/common/dao/InterviewTimeRepository.java
@@ -0,0 +1,7 @@
+package com.example.daedongyeojido_server.domain.club.common.dao;
+
+import com.example.daedongyeojido_server.domain.club.common.domain.InterviewTime;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InterviewTimeRepository extends JpaRepository<InterviewTime, Long> {
+}
