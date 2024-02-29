13b83235acda848f69db31211a232a9b6d6d14c3
dkflfkd53 <b57062562@gmail.com>
2024-02-29 14:46:25 +0900

    🌧 :: 지원서 페이지 조회 api service 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/report/application/QueryApplyPageService.java
@@ -0,0 +1,27 @@
+package com.example.daedongyeojido_server.domain.report.application;
+
+import com.example.daedongyeojido_server.domain.notice.dao.NoticeRepository;
+import com.example.daedongyeojido_server.domain.notice.domain.Notice;
+import com.example.daedongyeojido_server.domain.notice.exception.NoticeNotFoundException;
+import com.example.daedongyeojido_server.domain.report.dto.response.ApplyPageResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class QueryApplyPageService {
+
+    private final NoticeRepository noticeRepository;
+
+    @Transactional(readOnly = true)
+    public ApplyPageResponse queryApplyPage(Long noticeId) {
+        Notice notice = noticeRepository.findById(noticeId)
+                .orElseThrow(()-> NoticeNotFoundException.EXCEPTION);
+
+        return ApplyPageResponse.builder()
+                .clubName(notice.getClubName().getClubName())
+                .major(notice.getMajor())
+                .build();
+    }
+}
