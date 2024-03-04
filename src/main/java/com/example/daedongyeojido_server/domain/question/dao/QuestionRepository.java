cb85fe227b3f790729f276aa4eb5856331117682
dkflfkd53 <b57062562@gmail.com>
2024-03-04 12:12:15 +0900

    ♻️ :: findAllByClub 메서드 수정

--- src/main/java/com/example/daedongyeojido_server/domain/question/dao/QuestionRepository.java
+++ src/main/java/com/example/daedongyeojido_server/domain/question/dao/QuestionRepository.java
@@ -1,11 +1,11 @@
 package com.example.daedongyeojido_server.domain.question.dao;
 
 import com.example.daedongyeojido_server.domain.club.domain.Club;
-import com.example.daedongyeojido_server.domain.question.domain.QandA;
+import com.example.daedongyeojido_server.domain.question.domain.ClubQuest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface QuestionRepository extends JpaRepository<QandA, Long> {
-    List<QandA> findAllByClub(Club club);
+public interface QuestionRepository extends JpaRepository<ClubQuest, Long> {
+    List<ClubQuest> findAllByClub(Club club);
 }
