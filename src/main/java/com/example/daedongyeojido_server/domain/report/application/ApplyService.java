0a146c5f374695761a1c444fefc35f7955e3835e
dkflfkd53 <b57062562@gmail.com>
2024-03-05 12:30:44 +0900

    ♻️ :: 지원하기 api 예외처리

--- src/main/java/com/example/daedongyeojido_server/domain/report/application/ApplyService.java
+++ src/main/java/com/example/daedongyeojido_server/domain/report/application/ApplyService.java
@@ -3,10 +3,12 @@ package com.example.daedongyeojido_server.domain.report.application;
 import com.example.daedongyeojido_server.domain.notice.dao.NoticeRepository;
 import com.example.daedongyeojido_server.domain.notice.domain.Notice;
 import com.example.daedongyeojido_server.domain.notice.exception.NoticeNotFoundException;
+import com.example.daedongyeojido_server.domain.report.dao.CustomReportRepository;
 import com.example.daedongyeojido_server.domain.report.dao.ReportRepository;
 import com.example.daedongyeojido_server.domain.report.domain.Report;
 import com.example.daedongyeojido_server.domain.report.domain.enums.PassingResult;
 import com.example.daedongyeojido_server.domain.report.dto.request.ApplyRequest;
+import com.example.daedongyeojido_server.domain.report.exception.AlreadyApplyUserException;
 import com.example.daedongyeojido_server.domain.user.application.facade.UserFacade;
 import com.example.daedongyeojido_server.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
@@ -19,9 +21,11 @@ public class ApplyService {
 
     private final UserFacade userFacade;
 
-    private final ReportRepository reportRepository;
-
     private final NoticeRepository noticeRepository;
+
+    private final CustomReportRepository customReportRepository;
+
+    private final ReportRepository reportRepository;
 
     @Transactional
     public void apply(ApplyRequest request) {
@@ -29,6 +33,8 @@ public class ApplyService {
 
         Notice notice = noticeRepository.findById(request.getNoticeId())
                 .orElseThrow(() -> NoticeNotFoundException.EXCEPTION);
+
+        if(!(customReportRepository.findClubReport(notice.getClubName()).isEmpty())) throw AlreadyApplyUserException.EXCEPTION;
 
         reportRepository.save(
                 Report.builder()
