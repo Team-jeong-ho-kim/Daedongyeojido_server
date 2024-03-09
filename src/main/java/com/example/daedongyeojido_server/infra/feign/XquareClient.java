0f666040d5bb1ec64cd98b453b2e831a88c3b882
dkflfkd53 <b57062562@gmail.com>
2024-03-09 15:29:18 +0900

    ♻️ :: xquareUser 메서드 수정

--- src/main/java/com/example/daedongyeojido_server/infra/feign/XquareClient.java
+++ src/main/java/com/example/daedongyeojido_server/infra/feign/XquareClient.java
@@ -1,13 +1,15 @@
 package com.example.daedongyeojido_server.infra.feign;
 
+import com.example.daedongyeojido_server.infra.feign.dto.request.XquareUserRequest;
 import com.example.daedongyeojido_server.infra.feign.dto.response.XquareUserResponse;
+import jakarta.validation.Valid;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "xquare-client", url = "${key.xquare-api-url}")
 public interface XquareClient {
 
-    @GetMapping("/{accountId}")
-    XquareUserResponse xquareUser(@PathVariable String accountId);
+    @GetMapping("/user-data")
+    XquareUserResponse xquareUser(@RequestBody @Valid XquareUserRequest request);
 }
