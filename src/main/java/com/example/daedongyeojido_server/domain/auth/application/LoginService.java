19d5d0eec9196c05286b0d050eac97668ebb58e7
dkflfkd53 <b57062562@gmail.com>
2024-03-20 22:49:33 +0900

    ♻️ :: 로그인 api 수정

--- src/main/java/com/example/daedongyeojido_server/domain/auth/application/LoginService.java
+++ src/main/java/com/example/daedongyeojido_server/domain/auth/application/LoginService.java
@@ -5,11 +5,14 @@ import com.example.daedongyeojido_server.domain.auth.dto.response.LoginResponse;
 import com.example.daedongyeojido_server.domain.user.dao.UserRepository;
 import com.example.daedongyeojido_server.domain.user.domain.User;
 import com.example.daedongyeojido_server.domain.user.domain.enums.Part;
+import com.example.daedongyeojido_server.domain.user.exception.PasswordMisMatchException;
+import com.example.daedongyeojido_server.domain.user.exception.UserNotFoundException;
 import com.example.daedongyeojido_server.global.security.jwt.JwtTokenProvider;
 import com.example.daedongyeojido_server.infra.feign.XquareClient;
 import com.example.daedongyeojido_server.infra.feign.dto.response.XquareUserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,26 +26,35 @@ public class LoginService {
 
     private final XquareClient xquareClient;
 
+    private final PasswordEncoder passwordEncoder;
+
     @Value("${key.admin-id}")
     private String adminId;
 
     @Transactional
     public LoginResponse login(LoginRequest request) {
+        if(userRepository.existsByAccountId(request.getAccount_id())) {
+            User user = userRepository.findByAccountId(request.getAccount_id())
+                    .orElseThrow(()-> UserNotFoundException.EXCEPTION);
+
+            if(!passwordEncoder.matches(request.getPassword(), user.getPassword())) throw PasswordMisMatchException.EXCEPTION;
+
+            return jwtTokenProvider.receiveToken(request.getAccount_id());
+        }
+
         if(request.getAccount_id().equals(adminId)) {
-            User.builder()
+            userRepository.save(User.builder()
+                    .userId("admin")
                     .accountId(request.getAccount_id())
+                    .password(passwordEncoder.encode(request.getPassword()))
                     .name("관리자")
                     .part(Part.ADMIN)
-                    .build();
+                    .build());
 
             return jwtTokenProvider.receiveToken(request.getAccount_id());
         }
 
         XquareUserResponse xquareUserResponse = xquareClient.xquareUser(request.getAccount_id(), request.getPassword());
-
-        if(userRepository.existsByUserId(xquareUserResponse.getId().toString())) {
-            return jwtTokenProvider.receiveToken(xquareUserResponse.getAccount_id());
-        }
 
         String num = xquareUserResponse.getNum()<10 ? '0' + Integer.toString(xquareUserResponse.getNum()) : Integer.toString(xquareUserResponse.getNum());
         String classNumber = xquareUserResponse.getGrade().toString() + xquareUserResponse.getClass_num().toString() + num;
@@ -51,6 +63,7 @@ public class LoginService {
                 User.builder()
                     .userId(xquareUserResponse.getId().toString())
                     .accountId(xquareUserResponse.getAccount_id())
+                    .password(xquareUserResponse.getPassword())
                     .name(xquareUserResponse.getName())
                     .classNumber(classNumber)
                     .part(xquareUserResponse.getUser_role().equals("STU") ? Part.INDEPENDENT : Part.TEACHER)
