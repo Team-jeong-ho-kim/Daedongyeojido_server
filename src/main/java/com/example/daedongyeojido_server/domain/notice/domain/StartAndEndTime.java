259b13ed2aafb6e2e803a6d733a7d65e4e84386b
dkflfkd53 <b57062562@gmail.com>
2024-03-21 01:50:11 +0900

    🌧 :: 시작시간 마감시간 entity 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/notice/domain/StartAndEndTime.java
@@ -0,0 +1,33 @@
+package com.example.daedongyeojido_server.domain.notice.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class StartAndEndTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "start_day", nullable = false)
+    private LocalDate startDay;
+
+    @Column(name = "end_day", nullable = false)
+    private LocalDate endDay;
+}
