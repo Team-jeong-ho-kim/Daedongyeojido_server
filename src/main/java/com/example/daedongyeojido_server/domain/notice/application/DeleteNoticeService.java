54b6aa6fd5e73697b499429fdaaa1ea3f28fcab5
dkflfkd53 <b57062562@gmail.com>
2024-03-21 02:12:36 +0900

    ♻️ :: 공고 삭제 api 수정

--- src/main/java/com/example/daedongyeojido_server/domain/notice/application/DeleteNoticeService.java
+++ src/main/java/com/example/daedongyeojido_server/domain/notice/application/DeleteNoticeService.java
@@ -1,6 +1,10 @@
 package com.example.daedongyeojido_server.domain.notice.application;
 
+import com.example.daedongyeojido_server.domain.notice.application.facade.NoticeFacade;
+import com.example.daedongyeojido_server.domain.notice.dao.FieldRepository;
 import com.example.daedongyeojido_server.domain.notice.dao.NoticeRepository;
+import com.example.daedongyeojido_server.domain.notice.dao.StartEndEndTimeRepository;
+import com.example.daedongyeojido_server.domain.notice.domain.Notice;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,10 +13,20 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class DeleteNoticeService {
 
+    private final NoticeFacade noticeFacade;
+
     private final NoticeRepository noticeRepository;
+
+    private final FieldRepository fieldRepository;
+
+    private final StartEndEndTimeRepository startEndEndTimeRepository;
 
     @Transactional
     public void deleteNotice(Long noticeId) {
+        Notice notice = noticeFacade.noticeFacade(noticeId);
+
+        fieldRepository.deleteAllByNotice(notice);
+        startEndEndTimeRepository.deleteAllByNotice(notice);
         noticeRepository.deleteById(noticeId);
     }
 }
