0514524288b6eca01a52d9c6e96fd92e80a6ea27
dkflfkd53 <b57062562@gmail.com>
2024-03-21 16:16:25 +0900

    🌧 :: 지원서 질문 entity 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/report/domain/ReportQuest.java
@@ -0,0 +1,38 @@
+package com.example.daedongyeojido_server.domain.report.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ReportQuest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "question", nullable = false, length = 30)
+    private String question;
+
+    @Column(name = "answer", length = 200)
+    private String answer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "report", nullable = false)
+    private Report report;
+}
