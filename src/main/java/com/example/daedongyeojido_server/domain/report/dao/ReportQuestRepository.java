a4af0e8e25523645dd456068fb333ded8a70d80c
dkflfkd53 <b57062562@gmail.com>
2024-03-23 00:12:20 +0900

    🌧 :: 지원서 질문 repository 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/report/dao/ReportQuestRepository.java
@@ -0,0 +1,7 @@
+package com.example.daedongyeojido_server.domain.report.dao;
+
+import com.example.daedongyeojido_server.domain.report.domain.ReportQuest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportQuestRepository extends JpaRepository<ReportQuest, Long> {
+}
