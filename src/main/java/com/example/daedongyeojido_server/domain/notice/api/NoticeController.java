49c608a6cb5557e956e2fb462011f9a1a4a0b86c
dkflfkd53 <b57062562@gmail.com>
2024-03-22 20:14:11 +0900

    ♻️ :: 공고 컨트롤러 수정

--- src/main/java/com/example/daedongyeojido_server/domain/notice/api/NoticeController.java
+++ src/main/java/com/example/daedongyeojido_server/domain/notice/api/NoticeController.java
@@ -1,12 +1,18 @@
 package com.example.daedongyeojido_server.domain.notice.api;
 
+import com.example.daedongyeojido_server.domain.notice.application.AddNoticeQuestService;
 import com.example.daedongyeojido_server.domain.notice.application.CreateNoticeService;
 import com.example.daedongyeojido_server.domain.notice.application.DeleteNoticeService;
 import com.example.daedongyeojido_server.domain.notice.application.QueryAllNoticeService;
+import com.example.daedongyeojido_server.domain.notice.application.QueryApplyPageService;
 import com.example.daedongyeojido_server.domain.notice.application.QueryNoticeInfoService;
+import com.example.daedongyeojido_server.domain.notice.application.SelectMajorService;
 import com.example.daedongyeojido_server.domain.notice.dto.request.CreateNoticeRequest;
+import com.example.daedongyeojido_server.domain.notice.dto.request.MajorRequest;
+import com.example.daedongyeojido_server.domain.notice.dto.request.NoticeQuestRequest;
 import com.example.daedongyeojido_server.domain.notice.dto.response.NoticeInfoResponse;
 import com.example.daedongyeojido_server.domain.notice.dto.response.NoticeResponse;
+import com.example.daedongyeojido_server.domain.notice.dto.response.ApplyPageResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -21,6 +27,12 @@ public class NoticeController {
 
     private final CreateNoticeService createNoticeService;
 
+    private final SelectMajorService selectMajorService;
+
+    private final AddNoticeQuestService addNoticeQuestService;
+
+    private final QueryApplyPageService queryApplyPageService;
+
     private final QueryAllNoticeService queryAllNoticeService;
 
     private final QueryNoticeInfoService queryNoticeInfoService;
@@ -31,6 +43,24 @@ public class NoticeController {
     @PostMapping("/create")
     public void createNotice(@RequestBody @Valid CreateNoticeRequest request) {
         createNoticeService.createNotice(request);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/select")
+    public void selectMajor(@RequestBody @Valid MajorRequest request) {
+        selectMajorService.selectMajor(request);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/add-quest")
+    public void addNoticeQuest(@RequestBody @Valid NoticeQuestRequest request) {
+        addNoticeQuestService.addNoticeQuest(request);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/apply/{noticeId}")
+    public ApplyPageResponse queryApplyPage(@PathVariable Long noticeId) {
+        return queryApplyPageService.queryApplyPage(noticeId);
     }
 
     @ResponseStatus(HttpStatus.OK)
