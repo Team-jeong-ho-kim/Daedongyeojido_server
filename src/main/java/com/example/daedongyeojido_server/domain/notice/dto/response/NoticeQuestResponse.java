08df425477f065e61d8dcc86006e8c185d3f20b4
dkflfkd53 <b57062562@gmail.com>
2024-03-22 20:12:58 +0900

    🌧 :: 공고 질문 response 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/notice/dto/response/NoticeQuestResponse.java
@@ -0,0 +1,19 @@
+package com.example.daedongyeojido_server.domain.notice.dto.response;
+
+import com.example.daedongyeojido_server.domain.notice.domain.NoticeQuest;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class NoticeQuestResponse {
+
+    private Long id;
+
+    private String question;
+
+    public NoticeQuestResponse(NoticeQuest noticeQuest) {
+        id = noticeQuest.getId();
+        question = noticeQuest.getQuestion();
+    }
+}
