5252bb068d1b218b9f9da22030d05124b7a9d81e
dkflfkd53 <b57062562@gmail.com>
2024-03-22 20:16:50 +0900

    ♻️ :: userController 수정

--- src/main/java/com/example/daedongyeojido_server/domain/user/api/UserController.java
+++ src/main/java/com/example/daedongyeojido_server/domain/user/api/UserController.java
@@ -2,7 +2,9 @@ package com.example.daedongyeojido_server.domain.user.api;
 
 import com.example.daedongyeojido_server.domain.user.application.FindAllUserService;
 import com.example.daedongyeojido_server.domain.user.application.MyInfoService;
+import com.example.daedongyeojido_server.domain.user.application.UpdateProfileImageService;
 import com.example.daedongyeojido_server.domain.user.dto.request.PickSecretRequest;
+import com.example.daedongyeojido_server.domain.user.dto.request.ProfileImageRequest;
 import com.example.daedongyeojido_server.domain.user.dto.response.AllUserResponse;
 import com.example.daedongyeojido_server.domain.user.dto.response.MyInfoResponse;
 import jakarta.validation.Valid;
@@ -21,6 +23,8 @@ public class UserController {
 
     private final FindAllUserService findAllUserService;
 
+    private final UpdateProfileImageService updateProfileImageService;
+
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/my-info")
     public MyInfoResponse myInfo() {
@@ -31,5 +35,11 @@ public class UserController {
     @GetMapping("/all")
     public List<AllUserResponse> findAllUser(@RequestBody @Valid PickSecretRequest request) {
         return findAllUserService.findAllUser(request);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PatchMapping("/update-profile")
+    public void updateProfileImage(@RequestBody @Valid ProfileImageRequest request) {
+        updateProfileImageService.updateProfileImage(request);
     }
 }
