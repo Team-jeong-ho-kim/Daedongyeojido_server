19ee1cd29465da0ae4eaf5723264413c766747ef
dkflfkd53 <b57062562@gmail.com>
2024-03-22 20:07:28 +0900

    🌧 :: 공고 질문 repository 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/notice/dao/NoticeQuestRepository.java
@@ -0,0 +1,7 @@
+package com.example.daedongyeojido_server.domain.notice.dao;
+
+import com.example.daedongyeojido_server.domain.notice.domain.NoticeQuest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticeQuestRepository extends JpaRepository<NoticeQuest, Long> {
+}
