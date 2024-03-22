2a2b57b049b66bd00e47fe626940a75dafac0464
dkflfkd53 <b57062562@gmail.com>
2024-03-22 20:07:35 +0900

    🌧 :: 공고 질문 entity 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/notice/domain/NoticeQuest.java
@@ -0,0 +1,35 @@
+package com.example.daedongyeojido_server.domain.notice.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class NoticeQuest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "question", nullable = false, length = 30)
+    private String question;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "notice", nullable = false)
+    private Notice notice;
+}
