21226f84b64b24fd2f57df2a88630805e09be397
dkflfkd53 <b57062562@gmail.com>
2024-03-22 16:58:24 +0900

    ♻️ :: 지원서 조회 api 수정

--- src/main/java/com/example/daedongyeojido_server/domain/report/application/QueryApplyPageService.java
+++ src/main/java/com/example/daedongyeojido_server/domain/report/application/QueryApplyPageService.java
@@ -4,6 +4,9 @@ import com.example.daedongyeojido_server.domain.notice.dao.NoticeRepository;
 import com.example.daedongyeojido_server.domain.notice.domain.Notice;
 import com.example.daedongyeojido_server.domain.notice.exception.NoticeNotFoundException;
 import com.example.daedongyeojido_server.domain.report.dto.response.ApplyPageResponse;
+import com.example.daedongyeojido_server.domain.report.dto.response.QuestionResponse;
+import com.example.daedongyeojido_server.domain.user.application.facade.UserFacade;
+import com.example.daedongyeojido_server.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,15 +15,21 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class QueryApplyPageService {
 
+    private final UserFacade userFacade;
+
     private final NoticeRepository noticeRepository;
 
     @Transactional(readOnly = true)
     public ApplyPageResponse queryApplyPage(Long noticeId) {
+        User user = userFacade.currentUser();
+
         Notice notice = noticeRepository.findById(noticeId)
                 .orElseThrow(() -> NoticeNotFoundException.EXCEPTION);
 
         return ApplyPageResponse.builder()
-                .clubName(notice.getClubName().getClubName())
+                .name(user.getName())
+                .classNumber(user.getClassNumber())
+                .questions(notice.getReportQuests().stream().map(QuestionResponse::new).toList())
                 .build();
     }
 }
