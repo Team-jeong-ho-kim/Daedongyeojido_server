cec55cea20dfa8e719ab2fb08b13eb28d87544c1
dkflfkd53 <b57062562@gmail.com>
2024-03-22 20:14:02 +0900

    ♻️ :: 동아리 맴버 response 수정

--- src/main/java/com/example/daedongyeojido_server/domain/club/common/dto/response/ClubMemberResponse.java
+++ src/main/java/com/example/daedongyeojido_server/domain/club/common/dto/response/ClubMemberResponse.java
@@ -1,5 +1,6 @@
 package com.example.daedongyeojido_server.domain.club.common.dto.response;
 
+import com.example.daedongyeojido_server.domain.notice.domain.enums.Major;
 import com.example.daedongyeojido_server.domain.user.domain.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,7 +11,16 @@ public class ClubMemberResponse {
 
     private String name;
 
+    private Major major;
+
+    private String oneLiner;
+
+    private String profileImageUrl;
+
     public ClubMemberResponse(User user) {
         name = user.getName();
+        major = user.getMajor();
+        oneLiner = user.getUserId();
+        profileImageUrl = user.getProfileImageUrl();
     }
 }
