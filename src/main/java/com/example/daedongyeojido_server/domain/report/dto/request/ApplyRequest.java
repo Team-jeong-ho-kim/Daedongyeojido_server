3dcecc2ac3698a06bdb4675da2a690ae6a1c5f5e
dkflfkd53 <b57062562@gmail.com>
2024-03-22 20:18:03 +0900

    ♻️ :: 지원하기 api request 수정

--- src/main/java/com/example/daedongyeojido_server/domain/report/dto/request/ApplyRequest.java
+++ src/main/java/com/example/daedongyeojido_server/domain/report/dto/request/ApplyRequest.java
@@ -7,26 +7,19 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ApplyRequest {
 
-    @NotBlank
-    @Size(max = 20, message = "한 줄 소개는 20글자 이하여야 합니다.")
-    private String oneLiner;
-
-    @NotBlank
-    @Size(max = 100, message = "자기소개는 100글자 이하여야 합니다.")
-    private String introduction;
-
-    @NotBlank
-    @Size(max = 100, message = "희망하는 전공은 100글자 이하여야 합니다.")
-    private String hopeMajor;
-
-    @NotBlank
-    @Size(max = 100, message = "배우고 싶은 것은 100글자 이하여야 합니다.")
-    private String learn;
-
     @NotNull
     private Long noticeId;
+
+    @NotBlank
+    @Size(max = 30, message = "자기소개는 300글자 이하여야 합니다.")
+    private String introduce;
+
+    @NotNull
+    private List<ReportQuestRequest> reportQuests;
 }
