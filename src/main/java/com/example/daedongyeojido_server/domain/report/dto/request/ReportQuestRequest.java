e259c9fe26cf572b048c8f7ad5ae971a29f82d70
dkflfkd53 <b57062562@gmail.com>
2024-03-22 20:13:10 +0900

    🌧 :: 지원서 질문 request 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/report/dto/request/ReportQuestRequest.java
@@ -0,0 +1,18 @@
+package com.example.daedongyeojido_server.domain.report.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ReportQuestRequest {
+
+    @NotNull
+    private String question;
+
+    @NotBlank
+    private String answer;
+}
