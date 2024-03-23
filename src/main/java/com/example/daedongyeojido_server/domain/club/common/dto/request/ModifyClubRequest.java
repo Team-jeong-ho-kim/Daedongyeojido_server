78cd449c1a1b4a2254b8987034d9be903b3c559c
dkflfkd53 <b57062562@gmail.com>
2024-03-24 00:52:12 +0900

    ♻️ :: 동아리 수정 request 수정

--- src/main/java/com/example/daedongyeojido_server/domain/club/common/dto/request/ModifyClubRequest.java
+++ src/main/java/com/example/daedongyeojido_server/domain/club/common/dto/request/ModifyClubRequest.java
@@ -22,16 +22,9 @@ public class ModifyClubRequest {
     @Size(max = 500, message = "소개 글은 500글자 이하로 작성하여야 합니다.")
     private String introduction;
 
-    @Size(max = 500, message = "프로젝트 소개는 300글자 이하로 작성하여야 합니다.")
-    private String project;
-
-    @Size(max = 500, message = "인재상은 300글자 이하로 작성하여야 합니다.")
-    private String weWant;
-
-    @Size(max = 500, message = "Q&A는 300글자 이하로 작성하여야 합니다.")
-    private String qAndA;
-
     private String clubImageUrl;
+
+    private String clubBannerUrl;
 
     @Size(max = 6)
     private List<String> tags;
