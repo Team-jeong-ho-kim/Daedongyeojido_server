389bcf7e3ca91dd04938829de279167b189ab004
dkflfkd53 <b57062562@gmail.com>
2024-03-24 01:52:30 +0900

    🌧 :: 동아리 수정 request 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/notice/dto/request/UpdateNoticeRequest.java
@@ -0,0 +1,47 @@
+package com.example.daedongyeojido_server.domain.notice.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UpdateNoticeRequest {
+
+    @NotNull
+    private Long noticeId;
+
+    @NotBlank
+    private String noticeTitle;
+
+    @NotBlank
+    private String noticeExplain;
+
+    @NotBlank
+    private String clubExplain;
+
+    @NotNull
+    private List<FieldRequest> fields;
+
+    @NotNull
+    private StartAndEndTimeRequest recruitDay;
+
+    @NotBlank
+    private String applyMethod;
+
+    @NotNull
+    private StartAndEndTimeRequest interviewDay;
+
+    @NotBlank
+    private String inquiry;
+
+    @NotBlank
+    private String weWant;
+
+    @NotBlank
+    private String assignment;
+}
