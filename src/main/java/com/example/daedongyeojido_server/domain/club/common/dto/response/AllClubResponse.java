1f194940845fa0e7fab95f0a7081cf54d4a9aeae
dkflfkd53 <b57062562@gmail.com>
2024-03-26 23:23:44 +0900

    ♻️ :: 동아리 전체조회 api 수정

--- src/main/java/com/example/daedongyeojido_server/domain/club/common/dto/response/AllClubResponse.java
+++ src/main/java/com/example/daedongyeojido_server/domain/club/common/dto/response/AllClubResponse.java
@@ -1,6 +1,7 @@
 package com.example.daedongyeojido_server.domain.club.common.dto.response;
 
 import com.example.daedongyeojido_server.domain.club.domain.Club;
+import com.example.daedongyeojido_server.domain.notice.dto.response.MajorResponse;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -16,12 +17,16 @@ public class AllClubResponse {
 
     private String clubImageUrl;
 
+    private List<MajorResponse> majors;
+
     private List<String> tags;
 
     public AllClubResponse(Club club) {
         clubName = club.getClubName();
         title = club.getTitle();
         clubImageUrl = club.getClubImageUrl();
+        if(club.getNotice() == null) majors = null;
+        else majors = club.getNotice().getFields().stream().map(MajorResponse::new).toList();
         tags = club.getTags().subList(0, Math.min(3, club.getTags().size()));
     }
 }
