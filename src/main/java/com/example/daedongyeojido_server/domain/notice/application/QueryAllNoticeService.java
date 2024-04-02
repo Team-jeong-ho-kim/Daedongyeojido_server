b35975f3c907b94ea6c775d932d53750a3fde7f9
dkflfkd53 <b57062562@gmail.com>
2024-04-02 23:05:42 +0900

    ♻️ :: 공고 전체조회 api 수정

--- src/main/java/com/example/daedongyeojido_server/domain/notice/application/QueryAllNoticeService.java
+++ src/main/java/com/example/daedongyeojido_server/domain/notice/application/QueryAllNoticeService.java
@@ -1,7 +1,11 @@
 package com.example.daedongyeojido_server.domain.notice.application;
 
 import com.example.daedongyeojido_server.domain.notice.dao.NoticeRepository;
+import com.example.daedongyeojido_server.domain.notice.domain.Notice;
 import com.example.daedongyeojido_server.domain.notice.dto.response.NoticeResponse;
+import com.example.daedongyeojido_server.domain.report.dao.ReportRepository;
+import com.example.daedongyeojido_server.domain.user.application.facade.UserFacade;
+import com.example.daedongyeojido_server.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,11 +19,27 @@ public class QueryAllNoticeService {
 
     private final NoticeRepository noticeRepository;
 
+    private final UserFacade userFacade;
+
+    private final ReportRepository reportRepository;
+
     @Transactional(readOnly = true)
     public List<NoticeResponse> queryAllNotice() {
-        return noticeRepository.findAll()
+        List<Notice> allNotice = noticeRepository.findAll();
+
+        User user = userFacade.currentUser();
+
+        List<NoticeResponse> noticeResponses = allNotice
                 .stream()
                 .map(NoticeResponse::new)
                 .collect(Collectors.toList());
+
+        for (int i = 0; i < noticeResponses.size(); i++) {
+            if (!(reportRepository.findAllByClassNumberAndNotice(user.getClassNumber(), allNotice.get(i)).isEmpty())) {
+                noticeResponses.get(i).apply();
+            }
+        }
+
+        return noticeResponses;
     }
 }
--- src/main/java/com/example/daedongyeojido_server/domain/notice/dto/response/NoticeResponse.java
+++ src/main/java/com/example/daedongyeojido_server/domain/notice/dto/response/NoticeResponse.java
@@ -1,5 +1,6 @@
 package com.example.daedongyeojido_server.domain.notice.dto.response;
 
+import com.example.daedongyeojido_server.domain.notice.domain.Field;
 import com.example.daedongyeojido_server.domain.notice.domain.Notice;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,15 +15,21 @@ public class NoticeResponse {
 
     private String noticeTitle;
 
+    private String major;
+
     private StartAndEndTimeResponse recruitDay;
 
-    private String clubImageUrl;
+    private Boolean isApply = false;
 
     public NoticeResponse(Notice notice) {
         id = notice.getNoticeId();
         clubName = notice.getClubName().getClubName();
         noticeTitle = notice.getNoticeTitle();
+        major = notice.getFields().stream().map(Field::getMajor).toString();
         recruitDay = new StartAndEndTimeResponse(notice.getRecruitDay().getStartDay(), notice.getRecruitDay().getEndDay());
-        clubImageUrl = notice.getClubName().getClubImageUrl();
+    }
+
+    public void apply() {
+        isApply = true;
     }
 }
