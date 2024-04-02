d55b07b87ec190467142b72f81a3b559cba5b6ba
dkflfkd53 <b57062562@gmail.com>
2024-04-02 23:05:26 +0900

    ♻️ :: 지원 취소하기 api 수정

--- src/main/java/com/example/daedongyeojido_server/domain/report/application/CancelApplyService.java
+++ src/main/java/com/example/daedongyeojido_server/domain/report/application/CancelApplyService.java
@@ -1,6 +1,10 @@
 package com.example.daedongyeojido_server.domain.report.application;
 
+import com.example.daedongyeojido_server.domain.report.application.facade.ReportFacade;
 import com.example.daedongyeojido_server.domain.report.dao.ReportRepository;
+import com.example.daedongyeojido_server.domain.report.domain.Report;
+import com.example.daedongyeojido_server.domain.user.application.facade.UserFacade;
+import com.example.daedongyeojido_server.domain.user.exception.UserMismatchException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,10 +13,18 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class CancelApplyService {
 
+    private final ReportFacade reportFacade;
+
+    private final UserFacade userFacade;
+
     private final ReportRepository reportRepository;
 
     @Transactional
     public void cancelApply(Long reportId) {
-        reportRepository.deleteById(reportId);
+        Report report = reportFacade.reportFacade(reportId);
+
+        if(!userFacade.currentUser().getClassNumber().equals(report.getClassNumber())) throw UserMismatchException.EXCEPTION;
+
+        reportRepository.delete(report);
     }
 }
