1866823f955ba6425cb8f87c0b012bab7f6e0921
dkflfkd53 <b57062562@gmail.com>
2024-04-03 00:05:34 +0900

    🌧 :: find/exist by noticeTitle 메서드 추가

--- src/main/java/com/example/daedongyeojido_server/domain/notice/dao/NoticeRepository.java
+++ src/main/java/com/example/daedongyeojido_server/domain/notice/dao/NoticeRepository.java
@@ -4,4 +4,6 @@ import com.example.daedongyeojido_server.domain.notice.domain.Notice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
+    Notice findByNoticeTitle(String noticeTitle);
+    Boolean existsByNoticeTitle(String noticeTitle);
 }
