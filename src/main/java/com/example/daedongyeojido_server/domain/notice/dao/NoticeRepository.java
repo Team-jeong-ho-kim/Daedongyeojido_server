d10f5772903a2c52fefd36b938ea7ceec87df8a6
dkflfkd53 <b57062562@gmail.com>
2024-04-03 09:56:25 +0900

    🌧 :: existsByClubName 메서드 추가

--- src/main/java/com/example/daedongyeojido_server/domain/notice/dao/NoticeRepository.java
+++ src/main/java/com/example/daedongyeojido_server/domain/notice/dao/NoticeRepository.java
@@ -1,9 +1,13 @@
 package com.example.daedongyeojido_server.domain.notice.dao;
 
+import com.example.daedongyeojido_server.domain.club.domain.Club;
 import com.example.daedongyeojido_server.domain.notice.domain.Notice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
     Notice findByNoticeTitle(String noticeTitle);
+    
     Boolean existsByNoticeTitle(String noticeTitle);
+
+    Boolean existsByClubName(Club club);
 }
