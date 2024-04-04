65a108cfecb12ccbdb01731a339d17638b7080a6
dkflfkd53 <b57062562@gmail.com>
2024-04-04 21:32:36 +0900

    🌧 :: 동아리 질문 api controller 추가

--- src/main/java/com/example/daedongyeojido_server/domain/notice/api/NoticeController.java
+++ src/main/java/com/example/daedongyeojido_server/domain/notice/api/NoticeController.java
@@ -2,7 +2,9 @@ package com.example.daedongyeojido_server.domain.notice.api;
 
 import com.example.daedongyeojido_server.domain.notice.application.AddNoticeQuestService;
 import com.example.daedongyeojido_server.domain.notice.application.CreateNoticeService;
+import com.example.daedongyeojido_server.domain.notice.application.DeleteNoticeQuestService;
 import com.example.daedongyeojido_server.domain.notice.application.DeleteNoticeService;
+import com.example.daedongyeojido_server.domain.notice.application.QueryAllNoticeQuestService;
 import com.example.daedongyeojido_server.domain.notice.application.QueryAllNoticeService;
 import com.example.daedongyeojido_server.domain.notice.application.QueryApplyPageService;
 import com.example.daedongyeojido_server.domain.notice.application.QueryNoticeInfoService;
@@ -13,6 +15,7 @@ import com.example.daedongyeojido_server.domain.notice.dto.request.UpdateNoticeR
 import com.example.daedongyeojido_server.domain.notice.dto.response.AllNoticeResponse;
 import com.example.daedongyeojido_server.domain.notice.dto.response.ApplyPageResponse;
 import com.example.daedongyeojido_server.domain.notice.dto.response.NoticeInfoResponse;
+import com.example.daedongyeojido_server.domain.notice.dto.response.NoticeQuestResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -25,6 +28,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -41,9 +46,13 @@ public class NoticeController {
 
     private final QueryNoticeInfoService queryNoticeInfoService;
 
+    private final QueryAllNoticeQuestService queryAllNoticeQuestService;
+
     private final UpdateNoticeService updateNoticeService;
 
     private final DeleteNoticeService deleteNoticeService;
+
+    private final DeleteNoticeQuestService deleteNoticeQuestService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/create")
@@ -76,6 +85,12 @@ public class NoticeController {
     }
 
     @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/all-quest")
+    public List<NoticeQuestResponse> queryAllNoticeQuest(@PathVariable Long noticeId) {
+        return queryAllNoticeQuestService.queryAllNoticeQuest(noticeId);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
     @PatchMapping("/update")
     public void updateNotice(@RequestBody @Valid UpdateNoticeRequest request) {
         updateNoticeService.updateNotice(request);
@@ -85,5 +100,11 @@ public class NoticeController {
     @DeleteMapping("/delete/{noticeId}")
     public void deleteNotice(@PathVariable Long noticeId) {
         deleteNoticeService.deleteNotice(noticeId);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @DeleteMapping("/delete-noticeQuest/{noticeQuestId}")
+    public void noticeQuestDelete(@PathVariable Long noticeQuestId) {
+        deleteNoticeQuestService.deleteNoticeQuest(noticeQuestId);
     }
 }
