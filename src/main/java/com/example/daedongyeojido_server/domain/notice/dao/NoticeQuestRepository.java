81f2b6481a95c8c1431a4e5fc5620f0aab0ea4dc
dkflfkd53 <b57062562@gmail.com>
2024-04-04 21:32:25 +0900

    🌧 :: findByNotice 메서드 추가

--- src/main/java/com/example/daedongyeojido_server/domain/notice/dao/NoticeQuestRepository.java
+++ src/main/java/com/example/daedongyeojido_server/domain/notice/dao/NoticeQuestRepository.java
@@ -1,7 +1,11 @@
 package com.example.daedongyeojido_server.domain.notice.dao;
 
+import com.example.daedongyeojido_server.domain.notice.domain.Notice;
 import com.example.daedongyeojido_server.domain.notice.domain.NoticeQuest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NoticeQuestRepository extends JpaRepository<NoticeQuest, Long> {
+    List<NoticeQuest> findAllByNotice(Notice notice);
 }
