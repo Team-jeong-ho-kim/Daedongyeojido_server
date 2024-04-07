b14d016a3ce7399182e397db36931039aa6b5a9c
dkflfkd53 <b57062562@gmail.com>
2024-04-07 22:05:24 +0900

    🌧 :: delete 메서드 추가

--- src/main/java/com/example/daedongyeojido_server/domain/alarm/dao/AlarmRepository.java
+++ src/main/java/com/example/daedongyeojido_server/domain/alarm/dao/AlarmRepository.java
@@ -1,6 +1,8 @@
 package com.example.daedongyeojido_server.domain.alarm.dao;
 
 import com.example.daedongyeojido_server.domain.alarm.domain.Alarm;
+import com.example.daedongyeojido_server.domain.alarm.domain.enums.AlarmType;
+import com.example.daedongyeojido_server.domain.club.domain.Club;
 import com.example.daedongyeojido_server.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +10,6 @@ import java.util.List;
 
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
     List<Alarm> findAllByUser(User user);
+
+    void deleteByClubNameAndUserAndAlarmType(String clubName, User user, AlarmType alarmType);
 }
