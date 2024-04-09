64c68f3ca352baa7a9924d902e1d822516e22fd2
dkflfkd53 <b57062562@gmail.com>
2024-04-09 19:43:30 +0900

    🌧 :: ReportIdResponse 추가

---
+++ src/main/java/com/example/daedongyeojido_server/domain/alarm/dto/response/ReportIdResponse.java
@@ -0,0 +1,11 @@
+package com.example.daedongyeojido_server.domain.alarm.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ReportIdResponse {
+
+    private Long reportId;
+}
