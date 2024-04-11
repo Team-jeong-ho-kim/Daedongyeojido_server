562a91f58d6821933e6359aa88bcf9c6ecd05210
dkflfkd53 <b57062562@gmail.com>
2024-04-11 14:33:53 +0900

    ♻️ :: 공고 reuest 제약조건 수정

--- src/main/java/com/example/daedongyeojido_server/domain/notice/dto/request/CreateNoticeRequest.java
+++ src/main/java/com/example/daedongyeojido_server/domain/notice/dto/request/CreateNoticeRequest.java
@@ -2,7 +2,6 @@ package com.example.daedongyeojido_server.domain.notice.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,7 +13,6 @@ import java.util.List;
 public class CreateNoticeRequest {
 
     @NotBlank
-    @Size(min = 1, max = 20, message = "동아리 이름은 1글자 이상, 20글자 이하여야 합니다.")
     private String clubName;
 
     @NotBlank
--- src/main/java/com/example/daedongyeojido_server/domain/notice/dto/request/NoticeQuestRequest.java
+++ src/main/java/com/example/daedongyeojido_server/domain/notice/dto/request/NoticeQuestRequest.java
@@ -2,7 +2,6 @@ package com.example.daedongyeojido_server.domain.notice.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +14,5 @@ public class NoticeQuestRequest {
     private Long noticeId;
 
     @NotBlank
-    @Size(min = 1, max = 30, message = "질문은 1글자 이상, 30글자 이하여야 합니다.")
     private String question;
 }
