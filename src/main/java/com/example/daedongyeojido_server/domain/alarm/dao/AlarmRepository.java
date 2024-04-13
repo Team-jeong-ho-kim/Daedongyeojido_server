844bf7b074a69c5e3c94321c90fd3f7da1d58fbe
dkflfkd53 <b57062562@gmail.com>
2024-04-13 22:12:44 +0900

    🌧 :: deleteByReport 메서드 추가

--- src/main/java/com/example/daedongyeojido_server/domain/alarm/dao/AlarmRepository.java
+++ src/main/java/com/example/daedongyeojido_server/domain/alarm/dao/AlarmRepository.java
@@ -3,6 +3,7 @@ package com.example.daedongyeojido_server.domain.alarm.dao;
 import com.example.daedongyeojido_server.domain.alarm.domain.Alarm;
 import com.example.daedongyeojido_server.domain.alarm.domain.enums.AlarmType;
 import com.example.daedongyeojido_server.domain.club.domain.Club;
+import com.example.daedongyeojido_server.domain.report.domain.Report;
 import com.example.daedongyeojido_server.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -12,4 +13,6 @@ public interface AlarmRepository extends JpaRepository<Alarm, Long> {
     List<Alarm> findAllByUser(User user);
 
     void deleteByClubNameAndUserAndAlarmType(String clubName, User user, AlarmType alarmType);
+
+    void deleteByReport(Report report);
 }
