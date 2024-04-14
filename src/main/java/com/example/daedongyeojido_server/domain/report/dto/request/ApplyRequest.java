bc27a87029ece9823e2d06c7569564c6c2d43b19
dkflfkd53 <b57062562@gmail.com>
2024-04-14 21:53:43 +0900

    ♻️ :: 컬럼 제약조건 수정

--- src/main/java/com/example/daedongyeojido_server/domain/report/dto/request/ApplyRequest.java
+++ src/main/java/com/example/daedongyeojido_server/domain/report/dto/request/ApplyRequest.java
@@ -3,7 +3,6 @@ package com.example.daedongyeojido_server.domain.report.dto.request;
 import com.example.daedongyeojido_server.domain.notice.domain.enums.Major;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,7 +17,6 @@ public class ApplyRequest {
     private Long noticeId;
 
     @NotBlank
-    @Size(max = 30, message = "자기소개는 300글자 이하여야 합니다.")
     private String introduce;
 
     @NotNull
